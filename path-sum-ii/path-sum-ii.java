b4da77b405c86aab8af907b397c9ebe99bdb552f
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2021-12-31 21:30:41 +0530

    Time: 2 ms (32.47%), Space: 41.9 MB (9.62%) - LeetHub

--- path-sum-ii/path-sum-ii.java
+++ path-sum-ii/path-sum-ii.java
@@ -14,29 +14,33 @@
  * }
  */
 class Solution {
-    public void solution(TreeNode root,String list,List<List<Integer>>ans,int tv,int val){
+    public void solution(TreeNode root,List<Integer> list,List<List<Integer>>ans,int tv,int val){
+        
         if(root!=null && root.left==null && root.right==null){
-           list=list+","+root.val;val+=root.val;
-           if(val==tv){
-             
-              List<Integer>l=new ArrayList<>();
-              for(String s:list.split(",")){
-                 if(s.length()>0){
-                     l.add(Integer.parseInt(s));}
-              }
-              ans.add(l);}
-          return;
-        }
+             list.add(root.val);
+             if(val+root.val==tv){
+                List<Integer>l=new ArrayList<>();
+                for(int j:list){
+                    l.add(j);
+                }
+                ans.add(l);
+             }
+            return;
+         }
         if(root==null){
+            list.add(-1);
             return;
         }
-        solution(root.left,list+","+root.val,ans,tv,val+root.val);
-        solution(root.right,list+","+root.val,ans,tv,val+root.val);
+        list.add(root.val);
+        solution(root.left,list,ans,tv,val+root.val);
+        list.remove(list.size() - 1);
+        solution(root.right,list,ans,tv,val+root.val);
+        list.remove(list.size() - 1);
         
     }
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
          List<List<Integer>>ans=new ArrayList<>();
-         solution(root,"",ans,targetSum,0);
+         solution(root,new ArrayList(),ans,targetSum,0);
          return ans;
     }
 }
