306a87e7f9b657e01cd8bbea2be4b412ef7b259a
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-01-03 08:27:51 +0530

    Time: 26 ms (5.76%), Space: 38.9 MB (6.36%) - LeetHub

---
+++ path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> pathInZigZagTree(int label) {
+        List<Integer> result = new LinkedList<>();
+        if(label <= 0)
+            return result;
+        int level = 1;
+        while(Math.pow(2, level) - 1 < label)   level++;        
+        level--; // calculate the depth, 0 indexed, 0 is odd
+        while(level != 0) {
+            result.add(0, label);
+            int pos = label - (int) Math.pow(2, level); // calculate the position, 0 indexed
+            System.out.println(pos+" "+label+" "+level);
+            label = label - (pos + 1) - pos / 2;
+            level--;
+        }
+        result.add(0, 1);
+        return result;
+    }
+}
