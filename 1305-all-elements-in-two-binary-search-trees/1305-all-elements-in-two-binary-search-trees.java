cde5151d5bd42745234a6ba550f9d5b9da365a00
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-01-21 21:10:48 +0530

    Time: 44 ms (10.50%), Space: 89.3 MB (7.50%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -15,32 +15,26 @@
  */
 class Solution {
     //Total TC=O(n1+n2)+O(n1)+O(n2)
-    public void sortedBSTArr(TreeNode root,List<Integer>list){
+    public void sortedBSTArr(TreeNode root,Queue<Integer>list){
         if(root==null){          
             return ; 
         }                             //O(n)
         sortedBSTArr(root.left,list); //Inorder trabesal,gives sorted list of node values
-        list.add(root.val);
+        list.offer(root.val);
         sortedBSTArr(root.right,list);
     }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> list1=new ArrayList<>();
+        Queue<Integer> list1=new LinkedList<>();
         sortedBSTArr(root1,list1);  //for first bst
-        List<Integer> list2=new ArrayList<>();
+        Queue<Integer> list2=new LinkedList<>();
         sortedBSTArr(root2,list2);//for second bst
         List<Integer>ans=new ArrayList<>();
         int i=0;int j=0;
-        while(i<list1.size()&&j<list2.size()){
-            if(list1.get(i)<list2.get(j)){
-                ans.add(list1.get(i));i+=1;      //mergeing both list O(n1+n2)
-            }                                    
-            else if(list1.get(i)>list2.get(j)){
-                ans.add(list2.get(j));j+=1;
-             }
-             else{ans.add(list1.get(i));ans.add(list1.get(i));i+=1;j+=1;}
+        while(!list1.isEmpty() || !list2.isEmpty()){
+          if(list1.isEmpty()){ans.add(list2.poll());}
+          else if(list2.isEmpty()){ans.add(list1.poll());}
+          else{ans.add(list1.peek()<list2.peek()?list1.poll():list2.poll());}
         }
-        while(i<list1.size()){ans.add(list1.get(i));i+=1;}
-        while(j<list2.size()){ans.add(list2.get(j));j+=1;}
         return ans;
     }
 }
