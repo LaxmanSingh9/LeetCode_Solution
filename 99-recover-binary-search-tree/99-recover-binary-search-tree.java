d93e44543e7a7067186e8f4a8dbb2f95267e9948
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-01-23 20:47:16 +0530

    Time: 2 ms (91.02%), Space: 39.1 MB (83.58%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev=new TreeNode(Integer.MIN_VALUE);
+    TreeNode firstEle=null;TreeNode secondEle=null;     
+    public void inorderBST(TreeNode root){
+        if(root==null){return ;}
+        inorderBST(root.left);
+        
+        if(firstEle==null && prev.val>root.val){ //node will be previous node
+            firstEle=prev;
+        }
+        if(firstEle!=null && prev.val>root.val){//node  will be root
+            secondEle=root;
+        }             
+        prev=root;
+        inorderBST(root.right);                    
+    }
+    public void recoverTree(TreeNode root) {
+        inorderBST(root);
+        int buffer=firstEle.val;
+        firstEle.val=secondEle.val;
+        secondEle.val=buffer;
+        
+        
+    }
+}
