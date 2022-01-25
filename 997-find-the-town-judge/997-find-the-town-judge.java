4112f0f96ac82dfb7c6a21dcfc964899957f4bd7
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-01-25 21:04:43 +0530

    Time: 3 ms (91.95%), Space: 81.1 MB (64.14%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,37 +1,15 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(trust.length==0 &&n==1){
-            return 1;
+        int[]inDegree=new int[n+1];
+        for(int[]arr:trust){
+            inDegree[arr[0]]-=1;//For outdegree
+            inDegree[arr[1]]+=1;//For indegree
         }
-        Map<Integer,List<Integer>> map = new HashMap();
-        int[] visited = new int[n];
-        
-        for(int i = 0 ; i < n+1 ; i++){
-            map.put(i, new ArrayList());
-        }
-         //construct graph, add bidirectional vertex
-        for(int[] edge : trust){
-           map.get(edge[1]).add(edge[0]);
-           
-        }
-        int ans=-1;
-        for(Map.Entry<Integer,List<Integer>>m:map.entrySet()){
-            if(m.getValue().size()==n-1){
-                if(ans!=-1){
-                    return -1;
-                }
-                ans=m.getKey();
-                
+        for(int edge=1;edge<inDegree.length;edge+=1){
+           if(inDegree[edge]==n-1){
+                return edge;      //if indrgree equals n-1
             }
         }
-        if(ans==-1){
-            return -1;
-        }
-        for(int arr[]:trust){
-            if(ans==arr[0]){
-                return -1;
-            }
-        }
-        return ans;
+        return -1;
     }
 }
