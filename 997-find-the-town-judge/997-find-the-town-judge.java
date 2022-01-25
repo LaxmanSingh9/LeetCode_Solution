0325b21b1ea6424dceab5a989f0f4bd8b1f5a247
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-01-25 20:51:48 +0530

    Time: 42 ms (8.00%), Space: 47.1 MB (75.28%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if(trust.length==0 &&n==1){
+            return 1;
+        }
+        Map<Integer,List<Integer>> map = new HashMap();
+        int[] visited = new int[n];
+        
+        for(int i = 0 ; i < n+1 ; i++){
+            map.put(i, new ArrayList());
+        }
+         //construct graph, add bidirectional vertex
+        for(int[] edge : trust){
+           map.get(edge[1]).add(edge[0]);
+           
+        }
+        int ans=-1;
+        for(Map.Entry<Integer,List<Integer>>m:map.entrySet()){
+            System.out.println(m.getValue().toString());
+            if(m.getValue().size()==n-1){
+                if(ans!=-1){
+                    return -1;
+                }
+                ans=m.getKey();
+                
+            }
+        }
+        System.out.println(ans);
+        if(ans==-1){
+            return -1;
+        }
+        for(int arr[]:trust){
+            if(ans==arr[0]){
+                return -1;
+            }
+        }
+        return ans;
+    }
+}
