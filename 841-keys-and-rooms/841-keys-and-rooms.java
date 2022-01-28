aea605f67d81fa13b666cb4bc74fb27b781d75be
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-01-28 21:41:06 +0530

    Time: 1 ms (91.58%), Space: 44.3 MB (5.10%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.java
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -1,10 +1,10 @@
 class Solution {
     public void dfs(List<List<Integer>>graph,int s,int[]visited){
         visited[s]=1;
-        for(int edge:graph.get(s)){
+        for(int edge:graph.get(s))
             if(visited[edge]!=1)
                 dfs(graph,edge,visited);
-        }
+        
     }
     public boolean canVisitAllRooms(List<List<Integer>> graph){
       int n=graph.size();int[] visited = new int[n];
