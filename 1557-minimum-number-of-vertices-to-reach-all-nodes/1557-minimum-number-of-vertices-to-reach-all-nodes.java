5fcc59bfb6f78c23b1b24790ad5cdef46b28fc3c
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-01-28 20:18:49 +0530

    Time: 132 ms (5.06%), Space: 148.8 MB (6.03%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,16 +1,45 @@
 class Solution {
     //For an diretd acyclic graph the nodes with no in-degree must be in solution
     //Since those vertices are starting point of DAG.
+    
+    public void dfs(Map<Integer,List<Integer>>graph,int[] visited,int s,Map<Integer,Integer>map){
+        visited[s]=1;
+        for(int edge:graph.get(s)){
+            if(visited[edge]!=1){
+                dfs(graph,visited,edge,map);
+            }
+            else{
+                if(map.getOrDefault(edge,-1)!=-1){
+                    map.put(edge,-2);
+                }
+            }
+        }
+        
+        
+        
+    }
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-         int []inDegrees=new int[n];//Array to store indegree of vertices
-         for(List<Integer> edge:edges){
-             inDegrees[edge.get(1)]+=1;   
+         Map<Integer,Integer>map=new HashMap<>();
+         Map<Integer,List<Integer>>graph=new HashMap<>();
+         for(int i=0;i<n;i+=1){
+             graph.put(i,new ArrayList<>());
          }
-         
+         for(List<Integer>edgeArr:edges){
+             graph.get(edgeArr.get(0)).add(edgeArr.get(1));
+            
+         }
+         int []visited=new int[n];
+         for(int i=0;i<n;i+=1){
+             if(visited[i]!=1){
+                 dfs(graph,visited,i,map);
+                 map.put(i,1);
+             }
+         }
          List<Integer>answer=new ArrayList<>();
-         for(int idx=0;idx<n;idx+=1){
-             if(inDegrees[idx]==0)
-               answer.add(idx);  //store  vertices with zero indegree
+         for(Map.Entry<Integer,Integer>m:map.entrySet()){
+             if(m.getValue()==1){
+                 answer.add(m.getKey());
+             }
          }
          return answer;
     }
