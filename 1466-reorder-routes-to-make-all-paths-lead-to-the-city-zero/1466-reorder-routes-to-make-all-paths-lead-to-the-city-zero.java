9db032306a023242cee1eb91e4f68c60085ef81f
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-01-29 09:16:19 +0530

    Time: 111 ms (54.47%), Space: 134.4 MB (13.84%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int dfs(Map<Integer,List<Integer>>graph,int[]visited,int s){
+        visited[s]=1;
+        int cost=0;
+        for(int edge:graph.get(s)){
+            if(visited[Math.abs(edge)]!=1){
+                cost+=dfs(graph,visited,Math.abs(edge))+(edge>0?1:0);
+            }
+        }
+        return cost;
+    }
+    public int minReorder(int n, int[][] edges) {
+        Map<Integer,List<Integer>> graph = new HashMap();
+        int[] visited = new int[n];
+        
+        for(int i = 0 ; i < n ; i++){
+            graph.put(i, new ArrayList());
+        }
+         //construct graph, add bidirectional vertex
+        for(int[] edge : edges){
+           graph.get(edge[0]).add(edge[1]);
+           graph.get(edge[1]).add(-edge[0]);
+        }
+		//start dfs from start point
+       return dfs(graph,visited,0);
+       
+        
+    }
+}
