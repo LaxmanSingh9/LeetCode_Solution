d6aa508b7312d6d38f2892ec00d42f32140a2419
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-02-03 09:54:00 +0530

    Time: 72 ms (25.02%), Space: 83.9 MB (26.55%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.java
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -15,14 +15,37 @@ class Solution {
         return false;
         
     }
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n=graph.length;
-        List<Integer>ans=new ArrayList<>();int []v=new int[n];
+    public List<Integer> eventualSafeNodes(int[][] edges) {
+        int n=edges.length;
+        List<Integer>ans=new ArrayList<>();int []inDeg=new int[n];
+        Map<Integer,List<Integer>> graph = new HashMap();
+        for(int i=0;i<n;i++){
+            graph.put(i,new ArrayList());
+        }
         for(int i=0;i<n;i+=1){
-            if(hasCycle(i,graph,v)==false)
-              ans.add(i);
+            for(int edge:edges[i]){
+                graph.get(edge).add(i);
+                inDeg[i]+=1;
+            }
+              
+        }
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=0;i<n;i+=1){
+            if(inDeg[i]==0)
+                q.add(i);
+        }
+        while(!q.isEmpty()){
+            int s=q.poll();
+            ans.add(s);
+            for(int edge:graph.get(s)){
+                inDeg[edge]-=1;
+                if(inDeg[edge]==0){
+                    q.add(edge);
+                }
+            }
             
-        } 
+       }
+       Collections.sort(ans);
        return ans;
     }
 
