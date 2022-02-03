9f88ff5fd752b95e05666b8de462bd46123d251c
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-02-03 09:15:44 +0530

    Time: 8 ms (70.50%), Space: 64.9 MB (58.61%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.java
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.java
@@ -1,37 +1,29 @@
 class Solution {
   
-    public boolean hasCycle(int s,int[][]graph,int[]v,int []rest,int[]dp){
-        if(dp[s]==-1){
-            return true;
+    public boolean hasCycle(int s,int[][]graph,int[]v){
+        if(v[s]!=0){
+            return v[s]==1;
         }
         v[s]=1;
-        rest[s]=1;
         for(int e:graph[s]){
-            if(v[e]!=1 && hasCycle(e,graph,v,rest,dp)==true){
+            if(hasCycle(e,graph,v)==true){
                return true;
             }
-            else if(rest[e]==1){
+            else if(v[e]==1){
                 return true;
             }
         }
-        rest[s]=0;
+        v[s]=2;
         return false;
         
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n=graph.length;
-        
-        List<Integer>ans=new ArrayList<>();
-        int []v=new int[n];
-        int []rest=new int[n];
-        int []dp=new int[n];
+        List<Integer>ans=new ArrayList<>();int []v=new int[n];
         for(int i=0;i<n;i+=1){
-            if(hasCycle(i,graph,v,rest,dp)==false){
-                ans.add(i);
-            }
-            else{
-                dp[i]=-1;
-            }
+            if(hasCycle(i,graph,v)==false)
+              ans.add(i);
+            
         } 
        return ans;
     }
