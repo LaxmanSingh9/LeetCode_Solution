8ded03b4aa00efd2c0d4e64fd007a111dfd9c1dc
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-02-06 20:54:04 +0530

    Time: 62 ms (13.76%), Space: 53.9 MB (15.24%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,18 +1,25 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] edges) {
-        int [][]matrix=new int[n][n];
-        int[] count = new int[n];
+         Map<Integer,List<Integer>> graph = new HashMap();
+        int[] visited = new int[n];
+        
+        for(int i = 0 ; i < n ; i++){
+            graph.put(i, new ArrayList());
+        }
+         //construct graph, add bidirectional vertex
         for(int[] edge : edges){
-           count[edge[0]]+=1;
-           count[edge[1]]+=1;
-           matrix[edge[0]][edge[1]]=1;
-           matrix[edge[1]][edge[0]]=1;
+           graph.get(edge[0]).add(edge[1]);
+           graph.get(edge[1]).add(edge[0]);
         }
         int ans=Integer.MIN_VALUE;
 		for(int i=0;i<n;i+=1){
             for(int j=i+1;j<n;j+=1){
-                int totalEdgesBtwTwoVertices=count[i]+count[j]-(matrix[i][j]==1?1:0);
-                ans=Math.max(totalEdgesBtwTwoVertices,ans);
+                
+                int totalEdgesBetVertices=graph.get(i).size()+graph.get(j).size();
+                if(graph.get(i).contains(j)){
+                    totalEdgesBetVertices-=1;
+                }
+                ans=Math.max(totalEdgesBetVertices,ans);
             }
         }
         return ans;
