d53c4dbe19edc60b49c4e6d7369aeabafc450b1e
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-02-10 20:31:46 +0530

    Time: 31 ms (43.74%), Space: 62.4 MB (29.45%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -1,21 +1,22 @@
 class Solution {
-   public void bfs(Map<Integer,List<Integer>>graph,int n,int s,int []dist,int[][]mt){
+   public int bfs(Map<Integer,List<Integer>>graph,int n,int s,int []dist,int[][]mt){
          PriorityQueue<int[]>pq=new PriorityQueue<>((a, b) -> (a[1] - b[1]));
          dist[s]=0;int cnt=0;pq.add(new int[]{s,0});
+         int ans=0;
          while(!pq.isEmpty()){
            int []c=pq.remove();
            int u=c[0];int d1=c[1];
            if(dist[u]<d1){continue;}
-          
+           ans=dist[u];n-=1;
            for(int e:graph.get(u)){
                
                if(dist[e]>dist[u]+mt[u][e]){
                    dist[e]=dist[u]+mt[u][e];
                    pq.add(new int[]{e,dist[e]});
                }
-        }
-     }
-     
+         }
+      }
+      return n== 0 ? ans : -1;
 
   }
     public int networkDelayTime(int[][] edges, int n, int k) {
@@ -32,15 +33,8 @@ class Solution {
            graph.get(edge[0]-1).add(edge[1]-1);
            edgeWeight[edge[0]-1][edge[1]-1]=edge[2];
         }
-        bfs(graph,n,k-1,dist,edgeWeight);
-        int ans=Integer.MIN_VALUE;
-        for(int i=0;i<n;i+=1){
-            if(dist[i]==Integer.MAX_VALUE){
-                return -1;
-            }
-            
-            ans=Math.max(ans,dist[i]);
-        }
-        return ans;
+        return bfs(graph,n,k-1,dist,edgeWeight);
+        
+        
     }
 }
