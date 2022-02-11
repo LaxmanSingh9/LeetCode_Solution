d6295cf455cbe6c5f886cd50b691306628c8dc91
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-02-11 10:49:55 +0530

    Time: 47 ms (78.07%), Space: 47.7 MB (64.46%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -1,28 +1,17 @@
 class Solution {
     //dfs to check query validation
-    public List<Boolean> checkDependcies(int[][]queries,Map<Integer,Set<Integer>>prerequisitesMap){
-        int qlen=queries.length;List<Boolean>ans=new ArrayList<>();
-        for(int i=0;i<qlen;i+=1){
-            int d=queries[i][0];int dep=queries[i][1];
-            Set<Integer> set = prerequisitesMap.get(dep);
-            if(set.contains(d)){
-                ans.add(true);
-            }
-            else{
-                ans.add(false);
-            }
-            
-        }
-        return ans;
+    public boolean checkDependcies(List<Set<Integer>>lookUpArray,int d,int dep){
+         if(lookUpArray.get(dep).contains(d)){
+             return true;
+         }
+         return false;
         
     }
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
         int n=numCourses;
         Map<Integer,List<Integer>> graph = new HashMap();
-        Map<Integer, Set<Integer>> prerequisitesMap = new HashMap<>(); 
         for(int i = 0 ; i < n ; i++){
             graph.put(i, new ArrayList());
-              prerequisitesMap.put(i, new HashSet<>());
         }
         int []inDeg=new int[n];
         for(int edges[]:prerequisites){
@@ -37,21 +26,33 @@ class Solution {
             if(inDeg[i]==0)
                q.add(i);
         }
+        List<Set<Integer>>lookUpArray=new ArrayList<>();
+        for(int i=0;i<n;i+=1){
+            lookUpArray.add(new HashSet<>());
+        }
         while(q.isEmpty()==false){
             int v=q.poll();
            // System.out.println(v);
             for(int e:graph.get(v)){
-                prerequisitesMap.get(e).add(v);
-                prerequisitesMap.get(e).addAll(prerequisitesMap.get(v));
-               if(--inDeg[e]==0)
+                lookUpArray.get(e).add(v);
+                lookUpArray.get(e).addAll( lookUpArray.get(v));
+                if(--inDeg[e]==0)
                   q.add(e);
             }
         }
         //Topological Sort End
-        return checkDependcies(queries,prerequisitesMap);
-      
-       
         
+        //Logic To Check Query Validation
+       
+        int qlen=queries.length;List<Boolean>ans=new ArrayList<>();
+        for(int i=0;i<qlen;i+=1){
+            if(checkDependcies(lookUpArray,queries[i][0],queries[i][1])){
+                ans.add(true);}
+            else{
+                ans.add(false);}
+            
+        }
+        return ans;
         
         
         
