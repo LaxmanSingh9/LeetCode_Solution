6e055346b31c513ed5d7881b9ae860b849c54e21
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-02-13 07:31:20 +0530

    Time: 5 ms (78.66%), Space: 48.7 MB (16.99%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,4 +1,22 @@
 class Solution {
+    public boolean hasCycle(Map<Integer,List<Integer>>graph,int s,int[]vis,int []rest){
+        if(vis[s]==1){
+            return rest[s]==1;
+        }
+        vis[s]=1;
+        rest[s]=1;
+        for(int e:graph.get(s)){
+            if(hasCycle(graph,e,vis,rest)){
+                return true;
+            }
+        }
+        rest[s]=0;
+        return false;
+        
+        
+        
+        
+    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
          int n=numCourses;
         Map<Integer,List<Integer>> graph = new HashMap();
@@ -7,29 +25,16 @@ class Solution {
         }
         int []inDeg=new int[n];
         for(int edges[]:prerequisites){
-            graph.get(edges[0]).add(edges[1]);
-            inDeg[edges[1]]+=1;
+            graph.get(edges[1]).add(edges[0]);
+            
         }
-        
-        //Topologival Sort
-       
-        Queue<Integer>q=new LinkedList<>();
+        int []visited=new int[n];
+        int []rest=new int[n];
         for(int i=0;i<n;i+=1){
-            if(inDeg[i]==0)
-               q.add(i);
-        }
- 
-  
-        while(q.isEmpty()==false){
-            int v=q.poll();
-            n-=1;
-            for(int e:graph.get(v)){
-                
-                if(--inDeg[e]==0)
-                  q.add(e);
+            if(visited[i]!=1 && hasCycle(graph,i,visited,rest)){
+                return false;
             }
         }
-        return n==0?true:false;
-        
+        return true;
     }
 }
