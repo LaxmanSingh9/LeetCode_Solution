488b742299dec859c1c9dff1630d41f0241a763d
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-07 21:51:16 +0530

    Time: 3 ms (46.50%), Space: 48.3 MB (27.47%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,20 +1,26 @@
 class Solution {
-    public int solve(int n){
+    public int solve(int n,int []ans){
         if(n==0)
           return 0;
         if(n==1)
           return 1;
-    
-        if(n%2==1){
-            return 1+solve(n/2);
+        if(ans[n]!=-1){
+            return ans[n];
         }
-        return solve(n/2);
+        int val=0;
+        val=solve(n/2,ans);
+        if(n%2==1){
+            val+=1;
+        }
+        ans[n]=val;
+        return val;
         
     }
     public int[] countBits(int n) {
         int ans[]=new int[n+1];
+        Arrays.fill(ans,-1);
         for(int i=0;i<=n;i+=1){
-            ans[i]=solve(i);
+            ans[i]=solve(i,ans);
         }
         return ans;
     }
