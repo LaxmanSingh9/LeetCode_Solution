ce7be8eb0c28c06dc578f9da783d4957952d40bb
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-07 21:37:50 +0530

    Time: 25 ms (5.93%), Space: 48.9 MB (6.16%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int TotalonesInGivenrNumber(int n){
-        int ones=0;
-        int bit=0;
-        while(n!=0){
-           bit=n%2;
-           if(bit==1){
-              ones+=1; 
-           }
-           n=n/2;
+    public int solve(int n){
+        if(n==0)
+          return 0;
+        if(n==1)
+          return 1;
+    
+        if(n%2==1){
+            return 1+solve(n/2);
         }
-        return ones;
+        return solve(n/2);
         
     }
     public int[] countBits(int n) {
         int ans[]=new int[n+1];
         for(int i=0;i<=n;i+=1){
-            ans[i]=TotalonesInGivenrNumber(i);
+            ans[i]=solve(i);
         }
         return ans;
     }
