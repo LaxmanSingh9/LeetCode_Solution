6728a913d2c9e9118a45176cbb5adf9bf4801efe
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-07 22:07:01 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (6.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,18 +1,13 @@
 class Solution {
-    public int sumOfNumber(int n){
-        int sum=0;
-        for(int i=0;i<n;i+=1){
-            sum+=i;
-        }
-        return sum;
-    }
     public int fib(int n) {
-        if(n==0){
-           return 0;
+        if(n==0)
+          return 0;
+        int ans[]=new int[n+1];
+        ans[0]=0;ans[1]=1;
+        for(int i=2;i<=n;i+=1){
+            ans[i]=ans[i-2]+ans[i-1];
         }
-        if(n==1){
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
+        return ans[n];
+       
     }
 }
