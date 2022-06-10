99958c13a723656e4bf9a7715a388bcae9d35317
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-10 21:41:16 +0530

    Time: 1 ms (89.95%), Space: 41.7 MB (64.42%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,41 +1,20 @@
 class Solution {
  
-    public int solve(String s,String t,int i,int j,int[][]memo){
+    public boolean solve(String s,String t,int i,int j){
        if(i==-1 || j==-1){
-            return 0;
-        }
-        if(memo[i][j]!=-1){
-            return memo[i][j];
-        }
-        int val;
-        if(s.charAt(i)==t.charAt(j)){
-            val=1+solve(s,t,i-1,j-1,memo);
+           return i==-1?true:false;
+       }
+       if(s.charAt(i)==t.charAt(j)){
+            return solve(s,t,i-1,j-1);
         
         }
-        else{
-            int ans1=solve(s,t,i-1,j,memo);
-            int ans2=solve(s,t,i,j-1,memo);
-            val=Math.max(ans1,ans2);
-        }
-        memo[i][j]=val;
-        return val;
+        return solve(s,t,i,j-1);
         
     }
     
     public boolean isSubsequence(String s, String t) {
         int n=s.length();int m=t.length();
-        if(m==0 && n==0){
-            return true;
-        }
-        if(m==0 ||n==0){
-            return m==0?false:true;
-        }
-        int [][]memo=new int[n][m];
-        for(int ar[]:memo){
-            Arrays.fill(ar,-1);
-        }
-        int val=solve(s,t,0,0,memo);
-        return solve(s,t,n-1,m-1,memo)==n;
+        return solve(s,t,n-1,m-1);
         
     }
     
