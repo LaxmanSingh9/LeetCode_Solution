db9eb58754705cd45e3625800e53bfb3fc3f219c
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-10 21:06:59 +0530

    Time: 7 ms (8.57%), Space: 44.2 MB (5.48%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,44 @@
+class Solution {
+ 
+    public int solve(String s,String t,int i,int j,int[][]memo){
+       if(i>=s.length() || j>=t.length()){
+            return 0;
+        }
+        if(memo[i][j]!=-1){
+            return memo[i][j];
+        }
+        int val;
+        if(s.charAt(i)==t.charAt(j)){
+            val=1+solve(s,t,i+1,j+1,memo);
+        
+        }
+        else{
+            int ans1=solve(s,t,i+1,j,memo);
+            int ans2=solve(s,t,i,j+1,memo);
+            val=Math.max(ans1,ans2);
+        }
+        memo[i][j]=val;
+        return val;
+        
+    }
+    
+    public boolean isSubsequence(String s, String t) {
+        int n=s.length();int m=t.length();
+        if(n==0){
+            return true;
+        }
+        if(m==0){
+            return false;
+        }
+        int [][]memo=new int[n][m];
+        for(int ar[]:memo){
+            Arrays.fill(ar,-1);
+        }
+        int val=solve(s,t,0,0,memo);
+        System.out.println(val);
+        return solve(s,t,0,0,memo)==Math.min(m,n);
+        
+    }
+    
+    
+}
