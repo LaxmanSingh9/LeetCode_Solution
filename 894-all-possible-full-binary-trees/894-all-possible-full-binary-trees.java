b070fc888a4f0b29da95d23f7866723de0d5d56d
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-11 15:39:12 +0530

    Time: 9 ms (24.07%), Space: 60.2 MB (22.80%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.java
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.java
@@ -16,12 +16,15 @@
 class Solution {
     public List<TreeNode> allPossibleFBT(int N) {
         List<TreeNode> res = new ArrayList<>();
+        if(N<=0 || N%2==0){
+            return res;
+        }
         if(N==1){
             res.add(new TreeNode(0));
             return res;
         }
         N=N-1;
-        for(int i=1; i<=N;i+=2){
+        for(int i=1; i<=N;i+=1){
             List<TreeNode> left = allPossibleFBT(i);
             List<TreeNode> right = allPossibleFBT(N-i);
             for(TreeNode nl: left){
