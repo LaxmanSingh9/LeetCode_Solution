f06582c51a76116c95cba51b60b6069968d276e8
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-12 20:37:18 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (83.26%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,17 +1,25 @@
 class Solution {
-    public int solve(int places,int letterNum){
+    public int solve(int places,int letterNum,int[][]dp){
         if(places==0){
-            return 1;
+            return 1;//String is formed
         }
-        if(letterNum==0){
-            return 0;
+        if(letterNum<=0){
+            return 0; //Letter are finished
         }
-        return solve(places-1,letterNum)+solve(places,letterNum-1);
+        if(dp[places][letterNum]!=-1){
+            return dp[places][letterNum];
+        }
+        dp[places][letterNum]=solve(places-1,letterNum,dp)+solve(places,letterNum-1,dp);
+        return dp[places][letterNum];
         
     }
     
         
     public int countVowelStrings(int n) {
-        return solve(n,5);
+        int[][]dp=new int[n+1][6];
+        for(int []ar:dp){
+            Arrays.fill(ar,-1);
+        }
+        return solve(n,5,dp);
     }
 }
