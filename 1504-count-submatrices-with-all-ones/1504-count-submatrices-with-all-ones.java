6a607a9415b1af6df2440bade639179c6411b405
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-15 09:07:06 +0530

    Time: 12 ms (73.82%), Space: 54.6 MB (44.38%) - LeetHub

--- 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.java
@@ -3,19 +3,32 @@ class Solution {
         int m=matrix.length;
         int n=matrix[0].length;
         int ans=0;
+        int [][]dp=new int[m][n];
+        for(int i=0;i<m;i+=1){
+          for(int j=n-1;j>=0;j-=1){
+              if(j==n-1){
+                  dp[i][j]=matrix[i][j];
+              }
+              else if(matrix[i][j]==1){
+                  dp[i][j]=dp[i][j+1]+1;
+              }
+              else{
+                  dp[i][j]=0;
+              }
+              
+          }
+        } 
+        int minn,row;
         for(int i=0;i<m;i+=1){
             for(int j=0;j<n;j+=1){
-               int limit=n;
-               for(int i1=i;i1<m;i1+=1){
-                   for(int j1=j;j1<limit;j1+=1){
-                       if(matrix[i1][j1]==1){
-                           ans+=1;
-                       }
-                       else{
-                           limit=j1;
-                       }
-                   }
-               }
+                  row=i;
+                  minn=Integer.MAX_VALUE;
+                  while( row<m && dp[row][j]>0){
+                      minn=Math.min(dp[row][j],minn);
+                      row+=1;
+                      ans+=minn;
+                  }
+                 
             }
         }
         return ans;
