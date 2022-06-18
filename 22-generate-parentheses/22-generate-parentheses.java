935dd56faec7b883f6216c766a2697210ed3f9c6
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-18 16:50:35 +0530

    Time: 2 ms (65.88%), Space: 43.2 MB (70.72%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,36 +1,25 @@
 class Solution {
-    public boolean isValidParthesis(String string){
-        Stack<Character>stack=new Stack<>();
-        for(int i=0;i<string.length();i+=1){
-        if(!stack.empty() && string.charAt(i)!='(' &&string.charAt(i)!=stack.peek() ){
-                stack.pop();
-        }
-        else{
-                stack.push( string.charAt(i));
-          }
-       }
-       return stack.empty();
-    }
-    public List<String> ans=new ArrayList<>();
-    public Map<String,Integer>map=new HashMap<>();
-     public void solve(int n,int open,int close,String s){
-        if(open==n&&close==n){
-            if(isValidParthesis(s) && !map.containsKey(s)){
-                ans.add(s);map.put(s,1);
-            }
-            return ;
-        }
-        
-        if(open<n){
-            solve(n,open+1,close,s+"(");
-        }
-        if(close<open){
-            solve(n,open,close+1,s+")");
-        }
-       
-    }
     public List<String> generateParenthesis(int n) {
-         solve(n,0,0,"");
-         return ans;
+        List<String> ans = new ArrayList();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+
+        if (open < max) {
+            cur.append("(");
+            backtrack(ans, cur, open+1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(")");
+            backtrack(ans, cur, open, close+1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
     }
 }
