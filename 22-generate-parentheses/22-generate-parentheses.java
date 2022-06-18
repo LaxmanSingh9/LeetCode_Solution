bd56ae34633923f8109bc1d75ffbd7a7c1389712
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-18 17:02:09 +0530

    Time: 2 ms (65.88%), Space: 43.9 MB (40.84%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,25 +1,22 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList();
-        backtrack(ans, new StringBuilder(), 0, 0, n);
-        return ans;
+   
+    public List<String> ans=new ArrayList<>();
+    public void solve(int n,int open,int close,String s){
+        if(open==n&&close==n){
+             ans.add(s);
+              return ;
+        }
+        
+        if(open<n){
+            solve(n,open+1,close,s+"(");
+        }
+        if(close<open){
+            solve(n,open,close+1,s+")");
+        }
+       
     }
-
-    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max){
-        if (cur.length() == max * 2) {
-            ans.add(cur.toString());
-            return;
-        }
-
-        if (open < max) {
-            cur.append("(");
-            backtrack(ans, cur, open+1, close, max);
-            cur.deleteCharAt(cur.length() - 1);
-        }
-        if (close < open) {
-            cur.append(")");
-            backtrack(ans, cur, open, close+1, max);
-            cur.deleteCharAt(cur.length() - 1);
-        }
+    public List<String> generateParenthesis(int n) {
+         solve(n,0,0,"");
+         return ans;
     }
 }
