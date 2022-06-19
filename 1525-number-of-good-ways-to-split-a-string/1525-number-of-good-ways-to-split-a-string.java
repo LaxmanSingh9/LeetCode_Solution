5b9b86eda5258b6916ddac35675dcbf357a38dea
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-19 19:12:58 +0530

    Time: 27 ms (54.61%), Space: 45.8 MB (67.84%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
@@ -1,22 +1,19 @@
 class Solution {
+     /*
+      Suffix array and prefix array problem
+     */
      public int numSplits(String s) {
         int[]memo=new int[s.length()];
-        int []memo1=new int[s.length()];
         int ans=0;int n=s.length();
         Set<Character> set=new HashSet<>();
-        for(int i=0;i<n;i+=1){
+        for(int i=n-1;i>=0;i-=1){
             set.add(s.charAt(i));
             memo[i]=set.size();
         }
-       set=new HashSet<>();
-        for(int i=n-1;i>=0;i-=1){
-            set.add(s.charAt(i));
-            memo1[i]=set.size();
-        }
-         // System.out.println(Arrays.toString(memo));
-         //  System.out.println(Arrays.toString(memo1));
+        set.clear();
         for(int i=0;i<n;i+=1){
-            if(i+1!=n  &&  memo[i]==memo1[i+1]){
+            set.add(s.charAt(i));
+            if(i+1!=n && set.size()==memo[i+1]){
                 ans+=1;
             }
         }
