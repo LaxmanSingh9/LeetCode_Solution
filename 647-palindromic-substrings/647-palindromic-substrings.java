7b8ff80e5844472733c7ca194ce38d058b587bff
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-06-26 19:20:24 +0530

    Time: 46 ms (18.08%), Space: 51.4 MB (13.25%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,22 +1,37 @@
 import java.util.*;
 class Solution {
+     public int solve(int l,int r,char[] s,int [][]memo){
+        if(l==r ||r<l){// if lenght is 1
+            return 1;
+        }
+        if(memo[l][r]!=-1){
+            return memo[l][r];
+        }
+        memo[l][r]=s[l]!=s[r]?0:solve(l+1,r-1,s,memo);
+        solve(l+1,r,s,memo);
+        solve(l,r-1,s,memo);
+        return memo[l][r];
+        
+    }
+    
+    
     public int countSubstrings(String s) {
-       int totalPalindromeSubstrings=0,j=0,k=0;
-       int n=s.length();
-       char []arr=s.toCharArray();
-       for(int i=0;i<n;i+=1){  //This for odd length substring
-           j=i;k=i;
-           while(j>=0 && k<n && arr[j]==arr[k]){
-               totalPalindromeSubstrings+=1;j-=1;k+=1;
-           }
-       }
-       for(int i=0;i<n;i+=1){  //This for even length substring
-           j=i;k=i+1;
-           while(j>=0 && k<n && arr[j]==arr[k]){
-               totalPalindromeSubstrings+=1;j-=1;k+=1;
-
-           }
-       }
-       return totalPalindromeSubstrings;
+      int n=s.length();
+        int [][]memo=new int[n+2][n+2];
+        for(int []row:memo){
+            Arrays.fill(row,-1);
+        }
+        int ans=0;
+        solve(0,n-1,s.toCharArray(),memo);
+        for(int i=0;i<n;i+=1){
+           for(int j=i;j<n;j+=1){
+               if(memo[i][j]==1){
+                   ans+=1;
+               }
+           } 
+            
+        }
+        System.out.println(memo[1][1]);
+        return ans+n;
     }
 }
