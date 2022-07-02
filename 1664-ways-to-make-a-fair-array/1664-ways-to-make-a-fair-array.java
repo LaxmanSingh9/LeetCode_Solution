6da88dd46e765ab09be4321ebf624462857700cb
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-02 10:02:29 +0530

    Time: 10 ms (82.21%), Space: 82.3 MB (81.42%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
@@ -1,25 +1,13 @@
 class Solution {
-    public int waysToMakeFair(int[] nums) {
-        int ans=0,n=nums.length;
-        int oddSum=0,evenSum=0;
-        for(int i=0;i<n;i+=1){
-            if(i%2==0)
-               evenSum+=nums[i];
-            else
-               oddSum+=nums[i];
+     public int waysToMakeFair(int[] A) {
+        int res = 0, n = A.length, left[] = new int[2], right[] = new int[2];
+        for (int i = 0; i < n; i++)
+            right[i%2] += A[i]; 
+        for (int i = 0; i < n; i++) {
+            right[i%2] -= A[i];
+            if (left[0]+right[1] == left[1]+right[0]) res++;
+            left[i%2] += A[i];
         }
-        int evenSumUpto=0,oddSumUpto=0,currEven=0,currOdd=0;
-        
-        for(int i=0;i<n;i+=1){
-             currEven=i%2==0?nums[i]:0;
-             currOdd=i%2==1?nums[i]:0;
-             if(evenSumUpto+(oddSum-currOdd-oddSumUpto)==oddSumUpto+(evenSum-currEven-evenSumUpto)){
-                   ans+=1;   
-             }
-             evenSumUpto=i%2==0?evenSumUpto+nums[i]:evenSumUpto;
-             oddSumUpto=i%2==1?oddSumUpto+nums[i]:oddSumUpto;
-        }
-        return ans;
-        
+        return res;
     }
 }
