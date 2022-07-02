ce2fa7ba009e6ad36305b616624209fb56c77605
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-02 09:23:34 +0530

    Time: 16 ms (43.08%), Space: 83.3 MB (65.22%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
@@ -1,31 +1,23 @@
 class Solution {
     public int waysToMakeFair(int[] nums) {
         int ans=0,n=nums.length;
-        if(n==1 || n==2){
-            return n==1?1:0;
-        }
-        int []oddSum=new int[n];int []evenSum=new int[n];
+        int oddSum=0,evenSum=0;
         for(int i=0;i<n;i+=1){
-            if(i%2==0){
-                evenSum[i]=i==0?nums[i]:evenSum[i-1]+nums[i];
-                oddSum[i]=i==0?0:oddSum[i-1];
-            }
-            else{
-                oddSum[i]=oddSum[i-1]+nums[i];
-                evenSum[i]=i==0?0:evenSum[i-1];
-            }
-              
+            if(i%2==0)
+               evenSum+=nums[i];
+            else
+               oddSum+=nums[i];
         }
-        // System.out.println(Arrays.toString(evenSum));
-        //     System.out.println(Arrays.toString(oddSum));
+        int evenSumUpto=0,oddSumUpto=0,currEven=0,currOdd=0;
+        
         for(int i=0;i<n;i+=1){
-           if(i!=0 && evenSum[i-1]+oddSum[n-1]-oddSum[i]==oddSum[i-1]+evenSum[n-1]-evenSum[i]){
-               ans+=1;
-           }
-           else if(i==0 && evenSum[n-1]-nums[i]==oddSum[n-1]){
-               ans+=1;
-           }
-               
+             currEven=i%2==0?nums[i]:0;
+             currOdd=i%2==1?nums[i]:0;
+             if(evenSumUpto+(oddSum-currOdd-oddSumUpto)==oddSumUpto+(evenSum-currEven-evenSumUpto)){
+                   ans+=1;   
+             }
+             evenSumUpto=i%2==0?evenSumUpto+nums[i]:evenSumUpto;
+             oddSumUpto=i%2==1?oddSumUpto+nums[i]:oddSumUpto;
         }
         return ans;
         
