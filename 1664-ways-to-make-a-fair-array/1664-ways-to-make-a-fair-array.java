e86844ccca8a071310028b2963c7b38bc44563e5
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-02 10:10:34 +0530

    Time: 16 ms (43.08%), Space: 87.4 MB (62.45%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
@@ -6,8 +6,7 @@ class Solution {
         }
         for(int i=0;i<n;i+=1){
              right[i%2]-=nums[i];
-             if(left[0]+right[1]==left[1]+right[0])
-                 ans+=1;   
+             if(left[0]+right[1]==left[1]+right[0])ans+=1;   
              left[i%2]+=nums[i];
         }
         return ans;
