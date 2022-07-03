ab90b7ffd0bee7cfb79916e7a72f689a6eedb3c9
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-03 20:56:07 +0530

    Time: 1 ms (99.76%), Space: 40.7 MB (96.85%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
@@ -6,13 +6,16 @@ class Solution {
           return x+y;
         return x-y;
     }
-    public List<Integer>solve(String s){
+    public List<Integer>solve(String s ,Map<String,List<Integer>>map){
+        if(map.containsKey(s)){
+            return map.get(s);
+        }
         List<Integer>res=new ArrayList<>();
         for(int i=0;i<s.length();i+=1){
             char ch=s.charAt(i);
             if(ch=='*' || ch=='-' || ch=='+'){
-                List<Integer>left=solve(s.substring(0,i));
-                List<Integer>right=solve(s.substring(i+1));
+                List<Integer>left=solve(s.substring(0,i),map);
+                List<Integer>right=solve(s.substring(i+1),map);
                 for(int x:left){
                     for(int y:right)
                         res.add(evalExpress(ch,x,y));      
@@ -23,11 +26,13 @@ class Solution {
         if(res.size()==0){
             res.add(Integer.valueOf(s));
         }
+        map.put(s,res);
         return res;
         
         
     }
     public List<Integer> diffWaysToCompute(String expression) {
-          return solve(expression);
+          Map<String,List<Integer>>map=new HashMap<>();
+          return solve(expression,map);
     }
 }
