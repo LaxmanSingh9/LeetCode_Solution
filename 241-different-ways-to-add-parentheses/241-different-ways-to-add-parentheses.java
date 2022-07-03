e0a1813d2e1077064a157cf5374d5810ecb15a78
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-03 20:59:18 +0530

    Time: 4 ms (55.63%), Space: 42.4 MB (77.98%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
@@ -10,7 +10,7 @@ class Solution {
         if(map.containsKey(s)){
             return map.get(s);
         }
-        List<Integer>res=new ArrayList<>();
+        List<Integer>res=new LinkedList<>();
         for(int i=0;i<s.length();i+=1){
             char ch=s.charAt(i);
             if(ch=='*' || ch=='-' || ch=='+'){
