5cb1174cd49eb3e3449c595a704859b7b40ba85a
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-04 18:01:33 +0530

    Time: 18 ms (61.85%), Space: 54.6 MB (42.96%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -6,24 +6,16 @@ class Solution {
         for(int i=0;i<n;i+=1){
             freq[s.charAt(i) - 'a'] += 1;
         }
-        Arrays.sort(freq);
         Map<Integer,Integer>map=new HashMap<>();
         int currFreq=0,prevFreq=0;
-        for(int i=25;i>=0&&freq[i]!=0;i-=1){
+        for(int i=25;i>=0;i-=1){
             currFreq=freq[i];
-            if(map.containsKey(freq[i])){
-                if(map.containsKey(1)){
-                   for(int j=i;j>=0;j-=1)ans+=freq[j];
-                   break;
-                }
-                else{
-                   currFreq=prevFreq-1;
-                   ans+=freq[i]-currFreq;
-                }
+            while(map.containsKey(currFreq)){
+                currFreq-=1;
             }
+            if(currFreq<=0){ans+=freq[i];continue;}
             map.put(currFreq,1);
-            prevFreq=currFreq;
-            
+            ans+=freq[i]-currFreq;
         } 
         return ans;
     }
