da1a5eed894f22dba2a4cc7366ae105a33412171
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-04 09:51:25 +0530

    Time: 20 ms (51.59%), Space: 53.5 MB (84.87%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,34 +1,25 @@
+import java.util.*;
 class Solution {
     public int minDeletions(String s) {
-        Map<Character, Integer>map=new HashMap<>();
+        int []freq=new int[26];
         int n=s.length(),ans=0;
         for(int i=0;i<n;i+=1){
-            char ch=s.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-            }
-            else{
-                map.put(ch,1);
-            }
+            freq[s.charAt(i) - 'a'] += 1;
         }
-        List<Integer>freq=new ArrayList<>();
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-              freq.add(entry.getValue());
-        }
-        Collections.sort(freq, Collections.reverseOrder());
-        n=freq.size();
-        List<Integer>freq2=new LinkedList<>();
+        Arrays.sort(freq);
+        List<Integer>freq2=new ArrayList<>();
+       // System.out.println(Arrays.toString(freq));
         int currFreq=0,prevFreq=0;
-        for(int i=0;i<n;i+=1){
-            currFreq=freq.get(i);
-            if(freq2.contains(freq.get(i))){
+        for(int i=25;i>=0&&freq[i]!=0;i-=1){
+            currFreq=freq[i];
+            if(freq2.contains(freq[i])){
                 if(freq2.contains(1)){
-                   for(int j=i;j<n;j+=1)ans+=freq.get(j);
+                   for(int j=i;j>=0;j-=1)ans+=freq[j];
                    break;
                 }
                 else{
                    currFreq=prevFreq-1;
-                   ans+=freq.get(i)-currFreq;
+                   ans+=freq[i]-currFreq;
                 }
             }
             freq2.add(currFreq);
@@ -36,8 +27,5 @@ class Solution {
             
         } 
         return ans;
-        
-        
-        
     }
 }
