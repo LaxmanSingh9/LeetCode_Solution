f62bcad038130c6795adddf1018ef9139cc4ad26
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-04 08:47:22 +0530

    Time: 112 ms (13.55%), Space: 68.1 MB (15.56%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minDeletions(String s) {
+        Map<Character, Integer>map=new HashMap<>();
+        int n=s.length(),ans=0;
+        for(int i=0;i<n;i+=1){
+            char ch=s.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+            }
+            else{
+                map.put(ch,1);
+            }
+        }
+        List<Integer>freq=new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+              freq.add(entry.getValue());
+        }
+        Collections.sort(freq, Collections.reverseOrder());
+        n=freq.size();
+        List<Integer>freq2=new ArrayList<>();
+        for(int i=0;i<n;i+=1){
+            // System.out.println(ans+" "+freq.get(i));
+            if(freq2.contains(1)){
+                for(int j=i;j<n;j+=1){
+                      ans+=freq.get(j);
+                  }
+                  break;
+            }
+            else  if(freq2.contains(freq.get(i))){
+                int prev=freq.get(i);
+                freq.set(i,freq.get(i-1)-1);
+                ans+=prev-freq.get(i);
+                
+            }
+            freq2.add(freq.get(i));
+            
+        }
+        return ans;
+        
+        
+        
+    }
+}
