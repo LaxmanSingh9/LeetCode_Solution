af79e6e615333667102b05ab9ee33fbef26e09ab
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-12 08:47:55 +0530

    Time: 98 ms (9.90%), Space: 101.7 MB (9.90%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,29 +1,20 @@
 class Solution {
-    public int totalSumOfArr(int []neededTime){
-        int total=0;
-        for(int val:neededTime)total+=val;
-        return total;
-    }
-    public int solve(String s,int[]neededTime,int prev ,int i,int[][]dp){
-        if(i==neededTime.length)return 0;
-        
-        if(prev!=-1 && dp[i][prev]!=-1)return dp[i][prev];
-        int val;
-        if(prev!=-1 && prev==s.charAt(i)-'a'){
-            val=solve(s,neededTime,prev,i+1,dp);
-        }
-        else{
-            val=Math.max(solve(s,neededTime,prev,i+1,dp),
-                         solve(s,neededTime,s.charAt(i)-'a',i+1,dp)+neededTime[i]);
-        }
-        if(prev!=-1)
-          dp[i][prev]=val;
-        return val;
-    }
     public int minCost(String colors, int[] neededTime) {
-        int[] []dp=new int[colors.length()][27];
-        for(int []ar:dp)
-          Arrays.fill(ar,-1);
-        return totalSumOfArr(neededTime)-solve(colors,neededTime,-1,0,dp);
+        Stack<Integer> stack = new Stack<Integer>();
+        int i=0,ans=0,n=neededTime.length;
+        while(i!=n){
+            if(i==0)stack.push(i);
+            else if(stack.size()>0 && colors.charAt(stack.peek())==colors.charAt(i)){
+                 if(neededTime[stack.peek()]<neededTime[i]){
+                       ans+=neededTime[stack.pop()];stack.add(i);
+                  }
+                  else{
+                      ans+=neededTime[i];
+                  }
+            }
+            else stack.push(i);
+            i+=1;
+        }
+        return ans;
     }
 }
