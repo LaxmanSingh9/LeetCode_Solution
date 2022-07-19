d100a07d65e9ade1ba0978ddc6e181347a026523
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-19 09:24:50 +0530

    Time: 32 ms (44.63%), Space: 56.7 MB (14.46%) - LeetHub

--- 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.java
+++ 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.java
@@ -1,23 +1,25 @@
 class Solution {
     public long maximumSubsequenceCount(String text, String pattern) {
-        int totalZeroIndexChar=0,n=text.length();
+        int totalZeroIndexChar=0,totalOneIndexChar=0,n=text.length();
         int []dp=new int[n];long ans=0;
-        dp[0]=text.charAt(0)==pattern.charAt(1)?1:0;
-        for(int i=1;i<n;i+=1){
+        for(int i=n-1;i>=0;i-=1){
             if(text.charAt(i)==pattern.charAt(1)){
-                dp[i]=dp[i-1]+1;
+                dp[i]=i!=n-1?dp[i+1]+1:1;
             }
             else{
-                dp[i]=dp[i-1];
+                dp[i]=i!=n-1?dp[i+1]:0;
             }
         }
         for(int i=0;i<text.length();i+=1){
             if(text.charAt(i)==pattern.charAt(0)){
                 totalZeroIndexChar+=1;
-                ans+=(dp[n-1]-dp[i]);
+                ans+=dp[i];
             }
         }
-        ans+=Math.max(dp[n-1],totalZeroIndexChar);
+        if(pattern.charAt(0)!=pattern.charAt(1)){
+             ans+=Math.max(dp[0],totalZeroIndexChar);
+        }
+       
         return ans;
         
     }
