9df148002cef64815184b91f096c7fa6062ba10b
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-22 10:04:54 +0530

    Time: 16 ms (91.16%), Space: 46.1 MB (85.14%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,24 +1,31 @@
 class Solution {
     public String shortestCommonSupersequence(int [][]dp,String s1, String s2) {
-        int i = 0, j = 0;
-        String ans = "";
+        int i = 0, j = 0,l = 0;
+        char []ans=new char[s1.length()+s2.length()-dp[0][0]];
         while(i<s1.length() && j<s2.length()){
             if(s1.charAt(i)==s2.charAt(j)){
-                ans+=s1.charAt(i);
+                ans[l++]=s1.charAt(i);
                 i+=1;j+=1;
+                
             }
             else if(dp[i+1][j]<dp[i][j+1]){
-                ans+=s2.charAt(j);
+                ans[l++]=s2.charAt(j);
                 j+=1;
             }
             else{
-                ans+=s1.charAt(i);
+                ans[l++]=s1.charAt(i);
                 i+=1;
             }
             
             
         }
-        return ans + s1.substring(i) + s2.substring(j);
+        while(i<s1.length()){
+            ans[l++]=s1.charAt(i);i+=1;
+        }
+        while(j<s2.length()){
+            ans[l++]=s2.charAt(j);j+=1;
+        }
+        return new String(ans);
     }
     public int[][] LCS(String s1,String s2,int[][]dp){
        for(int i=s1.length()-1;i>=0;i-=1){
