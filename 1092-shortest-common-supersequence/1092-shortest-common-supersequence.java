8ba9f520b641c8c50ba57f6c8c1b32c6465bb79c
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-22 08:37:51 +0530

    Time: 41 ms (23.20%), Space: 51.5 MB (13.87%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,39 +1,31 @@
 class Solution {
     public String shortestCommonSupersequence(String lcs,String s1, String s2) {
         int i = 0, j = 0;
-        String res = "";
+        String ans = "";
         for (char c : lcs.toCharArray()) {
-            while(s1.charAt(i) != c){ res += s1.charAt(i);i+=1;}
-            while (s2.charAt(j) != c){res += s2.charAt(j);j+=1;}
-            res += c;i+=1;j+=1;
+            while(s1.charAt(i) != c){ ans += s1.charAt(i);i+=1;}
+            while (s2.charAt(j) != c){ans += s2.charAt(j);j+=1;}
+            ans += c;i+=1;j+=1;
         }
-        return res + s1.substring(i) + s2.substring(j);
+        return ans + s1.substring(i) + s2.substring(j);
     }
-    public String LCS(String s1,String s2,int i,int j,String[][]memo){
-        if(i==s1.length() || j==s2.length() ){
-            return "";
-        }
-        if(memo[i][j]!="A"){
-            return memo[i][j];
-        }
-        if(s1.charAt(i)==s2.charAt(j)){
-            memo[i][j]=s1.charAt(i)+LCS(s1,s2,i+1,j+1,memo);
-        }
-        else{
-            String smallAns1=LCS(s1,s2,i+1,j,memo);
-            String smallAns2=LCS(s1,s2,i,j+1,memo);
-            memo[i][j]=smallAns1.length()>smallAns2.length()?smallAns1:smallAns2;
-        }
-        return memo[i][j];
-
-        
+    public String LCS(String s1,String s2,String[][]dp){
+       for(int i=s1.length()-1;i>=0;i-=1){
+           for(int j=s2.length()-1;j>=0;j-=1){
+               if(s1.charAt(i)==s2.charAt(j)){
+                   dp[i][j]=s1.charAt(i)+dp[i+1][j+1];
+               }
+               else{
+                   dp[i][j]=dp[i+1][j].length()>dp[i][j+1].length()?dp[i+1][j]:dp[i][j+1];
+               }
+           }
+       }
+       return dp[0][0];
     }
     public String shortestCommonSupersequence(String str1, String str2) {
-         String [][]memo=new String[str1.length()][str2.length()];
-         for(String []ar:memo)
-             Arrays.fill(ar,"A");
-         String lcs=LCS(str1,str2,0,0,memo);
-         return shortestCommonSupersequence(lcs,str1,str2);
+         String [][]dp=new String[str1.length()+1][str2.length()+1];
+         for(String []ar:dp)Arrays.fill(ar,"");
+         return shortestCommonSupersequence(LCS(str1,str2,dp),str1,str2);
          
         
     }
