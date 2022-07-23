ae8b7ad91aa3f9f625d1b1a7ee56426952a15fe5
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-23 16:40:11 +0530

    Time: 122 ms (7.37%), Space: 129.2 MB (7.02%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,13 +1,10 @@
 class Solution {
-  public int[] solve(int []dp){
-     dp[0]=1;dp[1]=-1;dp[2]=1;dp[3]=1;dp[4]=2;dp[5]=2;
-     for(int i=6;i<dp.length;i+=1){
-         dp[i]=1+Math.min(dp[i-2],dp[i-3]);
-     }
-     return dp;
-  } 
   public int minimumRounds(int[] tasks) {
-      int []dp=new int[100015];
+      int []dp=new int[100010];
+      dp[0]=1;dp[1]=-1;dp[2]=1;dp[3]=1;dp[4]=2;dp[5]=2;
+      for(int i=6;i<=tasks.length;i+=1){
+         dp[i]=1+Math.min(dp[i-2],dp[i-3]);
+      }
       Map<Integer,Integer>map=new HashMap<>();
       for(int val:tasks){
           if(map.containsKey(val))
@@ -15,9 +12,9 @@ class Solution {
           else
              map.put(val,1);
       }
-      int ans=0,n;dp=solve(dp);
+      int ans=0;
       for (Map.Entry<Integer,Integer> entry : map.entrySet()){
-          n=entry.getValue();
+          int n=entry.getValue();
           if(dp[n]==-1){
               return -1;
           }
