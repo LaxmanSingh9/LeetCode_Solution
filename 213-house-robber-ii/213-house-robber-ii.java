bbe1695f254c883ea4d5bd4327e0eccf5be3b0d7
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-24 21:22:20 +0530

    Time: 1 ms (38.39%), Space: 41.1 MB (73.80%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -8,14 +8,11 @@ class Solution {
         dp[0][0]=nums[0];dp[0][1]=0;
         dp[1][0]=Math.max(nums[0],nums[1]);dp[1][1]=nums[1];
         for(int i=2;i<n;i+=1){
-            if(i==n-1){
+            if(i==n-1)
                 dp[i][0]=Math.max(dp[i-1][0],dp[i-2][0]);
-                 dp[i][1]=Math.max(dp[i-1][1],dp[i-2][1]+nums[i]);
-            }
-            else{
+            else
                dp[i][0]=Math.max(dp[i-1][0],dp[i-2][0]+nums[i]);
-               dp[i][1]=Math.max(dp[i-1][1],dp[i-2][1]+nums[i]);
-            }
+            dp[i][1]=Math.max(dp[i-1][1],dp[i-2][1]+nums[i]);
              
         }
         return Math.max(dp[n-1][0],dp[n-1][1]);
