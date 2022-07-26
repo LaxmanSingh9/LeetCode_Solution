f40fb6769024567f7ea7770284bf678a997f7199
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-26 21:59:48 +0530

    Time: 28 ms (88.94%), Space: 42.8 MB (83.79%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -7,23 +7,17 @@ class Solution {
     }
     public boolean canPartition(int[] nums) {
         int sumOfArr=calSumOfArr(nums);
-          if ((sumOfArr & 1) == 1)
-              return false;
-    
+        if((sumOfArr&1)==1)
+          return false;
         int n=nums.length,target=sumOfArr/2;
-        boolean [][]dp=new boolean[n+1][target+1];
-        for(int i=0;i<n+1;i+=1)
-            dp[i][0]=true;
-        for(int j=1;j<target+1;j+=1)
-            dp[0][j]=false;
-        for(int i=1;i<n+1;i+=1){
-            for(int j=1;j<target+1;j+=1){
-                dp[i][j]=dp[i-1][j];
-                if(j>=nums[i-1])
-                  dp[i][j]=(dp[i][j] || dp[i-1][j-nums[i-1]]);
+        boolean []dp=new boolean[target+1];
+        dp[0]=true;
+        for(int num:nums){
+            for(int i=target;i>=num;i-=1){
+                dp[i]=(dp[i]||dp[i-num]);
             }
         }
-        return dp[n][target];
+        return dp[target];
     }
 }
 
