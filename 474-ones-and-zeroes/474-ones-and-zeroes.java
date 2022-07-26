8de990b574aca5586052b4f47a59351ab934bf7c
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-26 11:25:06 +0530

    Time: 107 ms (37.53%), Space: 88.9 MB (24.15%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,42 +1,33 @@
 class Solution {
-    public int solve(String[] s,int i ,List<Integer>list,int m,int n,int[][][]memo){
-        if(m<0 || n<0 || i>=s.length){
-            return (m<0||n<0)?-1:0;
-        }
-        if(memo[i][m][n]!=-1){
-            return memo[i][m][n];
-        }
-        int[] arrofZerosAndOnes=cal(s[i],m,n);
-        int zeros=arrofZerosAndOnes[0],ones=arrofZerosAndOnes[1];
-        int ans1=solve(s,i+1,list,m-zeros,n-ones,memo)+1;
-        int ans2=solve(s,i+1,list,m,n,memo);
-        memo[i][m][n]= Math.max(ans1,ans2);
-        return  memo[i][m][n];
-        
-        
-    }
-    public int[] cal(String s,int m,int n){
+    public int cal(String s){
         Map<String,Integer>map=new HashMap<>();
-        int totalZeros=0,totalOnes=0;
+        int totalZeros=0;
         for(char c:s.toCharArray()){
-              if(c=='0')
-                 map.put("0",map.getOrDefault("0",0)+1);
-              else
-                 map.put("1",map.getOrDefault("1",0)+1);
-           }
+            if(c=='0')
+              map.put("0",map.getOrDefault("0",0)+1);
+        }
         totalZeros=map.getOrDefault("0",0);
-        totalOnes=map.getOrDefault("1",0);
-        int ans[]=new int[2];
-        ans[0]=totalZeros;ans[1]=totalOnes;
-        return ans;
+        return totalZeros;
     }
     public int findMaxForm(String[] strs, int m, int n) {
-        int len=strs.length;
-        int [][][]memo=new int[len+1][m+1][n+1];
-        for(int [][]ar:memo){
-            for(int []a:ar)
-              Arrays.fill(a,-1);
+        int [][][]dp=new int[strs.length+1][m+1][n+1];
+        int ans=0,zeros=0,ones=0;
+        for(int i=0;i<strs.length+1;i+=1){
+            if(i!=0){
+                 zeros=cal(strs[i-1]);ones=(strs[i-1].length())-zeros;
+            }
+            for(int j=0;j<m+1;j+=1){
+              for(int k=0;k<n+1;k+=1){
+                  if(i==0)
+                     dp[i][j][k]=0;
+                  else if(j>=zeros && k>=ones)
+                     dp[i][j][k] = Math.max(dp[i-1][j][k], dp[i-1][j-zeros][k-ones]+1);
+                 
+                 else
+                    dp[i][j][k]=dp[i-1][j][k];
+                }
+            }
         }
-        return solve(strs,0,new ArrayList<>(),m,n,memo);
+        return dp[strs.length][m][n];
     }
 }
