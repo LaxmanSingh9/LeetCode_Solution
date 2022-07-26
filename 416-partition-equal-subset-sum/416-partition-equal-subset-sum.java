c4680c008bd3e211e604afe845015a95e8111679
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-26 21:14:26 +0530

    Time: 166 ms (15.71%), Space: 69.3 MB (20.21%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int calSumOfArr(int []ar){
+        int ans=0;
+        for(int val:ar)
+            ans+=val;
+        return ans;
+    }
+    public int  solve(int arr[],int i,int target,int [][]memo){
+        if(target==0 || i>=arr.length)
+            return target==0?1:0;
+        if(memo[i][target]!=-1){
+            return memo[i][target];
+        }
+        int consist=0;
+        if(arr[i]<=target)
+          consist=solve(arr,i+1,target-arr[i],memo);
+        int skip=solve(arr,i+1,target,memo);
+        memo[i][target]= consist==1?consist:skip;
+        return memo[i][target];
+        
+        
+    }
+    public boolean canPartition(int[] nums) {
+        int sumOfArr=calSumOfArr(nums);
+        if(sumOfArr%2==1)
+            return false;
+        int [][]memo=new int[nums.length+1][sumOfArr/2+2];
+        for(int []ar:memo)Arrays.fill(ar,-1);
+        return solve(nums,0,sumOfArr/2,memo)==1?true:false;
+    }
+}
