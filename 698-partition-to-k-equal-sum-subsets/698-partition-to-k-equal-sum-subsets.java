f79929c4c7f044e3772df7fc1f6727fa1f8248c2
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-27 18:47:22 +0530

    Time: 2633 ms (8.28%), Space: 42.2 MB (50.28%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -1,4 +1,15 @@
-import java.util.*;
+/*
+   A given number can include  only in a subset
+   All the numbers are non-negative ,there are some minor changes in case of negative numbers
+   
+   Backtracing 
+   we are making all the placements for k subsets with sum/k target
+  Sorting reduces some unnesary computations by not going deeper stack call
+   
+   
+   
+   
+*/
 class Solution {
     public int getSumOfArr(int []arr){
         int ans=0;
@@ -6,26 +17,26 @@ class Solution {
         return ans;
     }
     public boolean solve(int []nums,int end,int target ,int curr ,int k,boolean[] vis){
-        if(k==1 || curr>target) //finds k-1 subset sum , will surely get the rest sum
+        if(k==1 || curr>target) //finds k-1 subset sum , will surely get the rest subset sum
             return k==1?true:false;
         if(curr==target)
-            return solve(nums,nums.length-1,target,0,k-1,vis);
+            return solve(nums,nums.length-1,target,0,k-1,vis); //Making further calls for  k-1  subsets & 
         for(int i=end;i>=0;i-=1){
             if(vis[i]==false){
                 vis[i]=true;
                 if(solve(nums,i-1,target,curr+nums[i],k,vis))
                   return true;  
-                vis[i]=false;
+                vis[i]=false; // Uncheck indexs with does not give target sums
              }
          }
-         return false;
+         return false; //in the end return false , can not find placements of number for k subsets
         
     }
     public boolean canPartitionKSubsets(int[] nums, int k) {
         int sum =getSumOfArr(nums);
         if(k <= 0 || sum%k != 0)return false;
         boolean []vis=new boolean[nums.length];
-        Arrays.sort(nums);
+        Arrays.sort(nums);  //
         return solve(nums,nums.length-1,sum/k,0,k,vis);
         
     }
