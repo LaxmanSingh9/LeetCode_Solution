e4d161a772b680d65fe6425bcbb746d0598719fa
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-31 08:50:13 +0530

    Time: 3 ms (88.04%), Space: 42.7 MB (18.51%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -2,24 +2,24 @@ class Solution {
     int ans=Integer.MAX_VALUE;
     public int solve(int []arr,int i,int target,int curr,int[][]memo){
         if(i>=arr.length){
-           return curr;
+           return target;
         }
-        if(memo[i][curr]!=-1)
-            return memo[i][curr];
-        int skip=Integer.MIN_VALUE,add=Integer.MIN_VALUE;
-        if(curr+arr[i]<=target){
-             add=solve(arr,i+1,target,curr+arr[i],memo);
+        if(memo[i][target]!=-1)
+            return memo[i][target];
+        int skip=Integer.MAX_VALUE,add=Integer.MAX_VALUE;
+        if(arr[i]<=target){
+             add=solve(arr,i+1,target-arr[i],curr+arr[i],memo);
         }
         skip=solve(arr,i+1,target,curr,memo);
-        memo[i][curr]=Math.max(add,skip);
-        return  memo[i][curr];
+        memo[i][target]=Math.min(add,skip);
+        return  memo[i][target];
     }
     public int lastStoneWeightII(int[] stones) {
         int sum=0;
         for(int val:stones)sum+=val;
         int [][]memo=new int[stones.length+1][sum/2+1];
         for(int []ar:memo)Arrays.fill(ar,-1);
-         int target=(sum>>1);
-        return sum-2*solve(stones,0,target,0,memo);
+        int target=(sum>>1);
+        return sum-2 *(target-solve(stones,0,target,0,memo)) ;
     }
 }
