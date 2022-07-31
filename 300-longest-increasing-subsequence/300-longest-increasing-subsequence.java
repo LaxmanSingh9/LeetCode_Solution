4b63321b980b38a7320a033f2de9c547b2fff47d
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-31 10:05:51 +0530

    Time: 1018 ms (5.00%), Space: 241 MB (6.94%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,23 +1,22 @@
 class Solution {
-  
-   public int solve(int[]nums,int i,int prev,int[][]dp){
-       if(i==nums.length)
-          return 0;
-       if(prev!=-1 && dp[i][prev]!=-1)
-          return dp[i][prev];
-       int exculde=solve(nums,i+1,prev,dp);
-       int include=0;
-       if(prev==-1 || nums[i]>nums[prev])
-          include=1+solve(nums,i+1,i,dp);
-       if(prev!=-1)
-          dp[i][prev]=Math.max(exculde,include);
-       return Math.max(exculde,include);
-   }
-   public int lengthOfLIS(int[] nums){
-        int [][]dp=new int[nums.length+1][nums.length+1];
-        for(int []ar:dp){
-            Arrays.fill(ar,-1);
-        }
-        return   solve(nums,0,-1,dp);
-   }
+    public int solve(int []nums,int i,int prev,int [][]memo){
+        if(i>=nums.length)
+            return 0; 
+        if(prev!=-1 && memo[i][prev]!=-1)
+            return memo[i][prev];
+        int add=0,skip=0;
+        if(prev==-1 || nums[prev]<nums[i])
+          add=solve(nums,i+1,i,memo)+1;
+        skip=solve(nums,i+1,prev,memo);
+        if(prev!=-1)
+           memo[i][prev]=Math.max(add,skip);
+        return Math.max(add,skip);
+    
+    }
+    public int lengthOfLIS(int[] nums) {
+        int [][]memo=new int[nums.length+1][nums.length+1];
+        for(int []ar:memo)
+           Arrays.fill(ar,-1);
+        return solve(nums,0,-1,memo);
+    }
 }
