e3834fadc8cafc381651b7aaa976e555f9a133a1
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-07-31 11:51:34 +0530

    Time: 90 ms (37.07%), Space: 44.1 MB (68.53%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,22 +1,14 @@
 class Solution {
-    public int solve(int []nums,int i,int prev,int [][]memo){
-        if(i>=nums.length)
-            return 0; 
-        if(prev!=-1 && memo[i][prev]!=-1)
-            return memo[i][prev];
-        int add=0,skip=0;
-        if(prev==-1 || nums[prev]<nums[i])//To add 1 length subsubseq
-            add=solve(nums,i+1,i,memo)+1;
-        skip=solve(nums,i+1,prev,memo);
-        if(prev!=-1)
-           memo[i][prev]=Math.max(add,skip);
-        return Math.max(add,skip);
-    
-    }
-    public int lengthOfLIS(int[] nums) {
-        int [][]memo=new int[nums.length+1][nums.length+1];
-        for(int []ar:memo)
-           Arrays.fill(ar,-1);
-        return solve(nums,0,-1,memo);
+  public int lengthOfLIS(int[] nums) {
+        int []dp=new int[nums.length+1];
+        Arrays.fill(dp,1);int ans=1;
+        for(int i=1;i<nums.length;i+=1){
+          for(int j=i;j>=0;j-=1){
+              if(nums[i]>nums[j])
+                dp[i]=Math.max(dp[i],dp[j]+1);
+          }
+          ans=Math.max(dp[i],ans);
+        }
+        return ans;
     }
 }
