3f2ab6d1c02fea36621405742ecffbd49170c433
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-01 09:36:26 +0530

    Time: 1 ms (41.15%), Space: 42 MB (26.28%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -26,9 +26,9 @@ class Solution {
         inorder(root);
         for(int i=0;i<list.size()-1;i+=1){
             list.get(i).right=list.get(i+1);
-            list.get(i).left=null;
+            list.get(i+1).left=null;
         }
-        list.get(list.size()-1).left=null;
+       // list.get(list.size()-1).left=null;
         return list.get(0);
     } 
 }
