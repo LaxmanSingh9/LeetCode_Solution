169d2cb4b57ac82b8f4a8cde7587d2254a4edaab
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-02 22:10:50 +0530

    Time: 2 ms (20.09%), Space: 51.4 MB (19.28%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.java
@@ -15,6 +15,8 @@
  */
 class Solution {
     public TreeNode solve(TreeNode root1,TreeNode root2){
+          if(root1==null){return root2;}
+          if(root2==null){return root1;}
           Queue<TreeNode>q1=new LinkedList<>();
           Queue<TreeNode>q2=new LinkedList<>();
           q1.add(root1);q2.add(root2);
@@ -25,13 +27,13 @@ class Solution {
               if(node1.left!=null && node2.left!=null){
                   q1.add(node1.left);q2.add(node2.left);
               }
+              else if(node2.left!=null) {
+                  node1.left=node2.left;
+              }
               if(node1.right!=null && node2.right!=null){
                   q1.add(node1.right);q2.add(node2.right);
               }
-              if(node1.left==null){
-                  node1.left=node2.left;
-              }
-              if(node1.right==null){
+              else if(node2.right!=null){
                   node1.right=node2.right;
               }
                   
@@ -41,8 +43,6 @@ class Solution {
           return root1;
     }
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if(root1==null){return root2;}
-        if(root2==null){return root1;}
         return  solve(root1,root2);
     }
 }
