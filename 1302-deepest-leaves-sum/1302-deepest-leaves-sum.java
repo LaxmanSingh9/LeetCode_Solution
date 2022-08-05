4af472efe1ccb5f790536f557c2b2e1c93c69c5d
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-05 21:37:16 +0530

    Time: 10 ms (38.94%), Space: 58.7 MB (39.01%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -15,21 +15,19 @@
  */
 class Solution {
     Map<Integer,Integer>map=new HashMap<>();
+    int maxLevel=0,ans=0;
     public void solve(TreeNode root ,int level){
         if(root==null){return;}
         map.put(level,map.getOrDefault(level,0)+root.val);
+        if(level>=maxLevel){
+          ans=map.get(level);maxLevel=level;
+        }
         solve(root.left,level+1);
         solve(root.right,level+1);
         
     }
     public int deepestLeavesSum(TreeNode root) {
-        int deepestLevel=0,ans=0;
         solve(root,0);
-        for(Map.Entry<Integer,Integer>m:map.entrySet()){
-            if(m.getKey()>=deepestLevel){
-               deepestLevel=m.getKey();ans=m.getValue();
-            }
-        }
         return ans;
         
     }
