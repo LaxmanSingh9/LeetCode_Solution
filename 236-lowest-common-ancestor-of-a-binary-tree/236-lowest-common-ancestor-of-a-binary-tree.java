7d3e4d76a8e40e42aa7edb94c8355b6591c08d05
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-05 08:44:25 +0530

    Time: 15 ms (20.72%), Space: 47.4 MB (49.27%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,6 +8,33 @@
  * }
  */
 class Solution {
+    public TreeNode  solve(TreeNode  root,TreeNode p,TreeNode q){
+        Queue<TreeNode>queue=new LinkedList<>();
+        queue.add(root);
+        Map<TreeNode,TreeNode>parent=new HashMap<>();
+        parent.put(root,null);
+        while(!parent.containsKey(p) || !parent.containsKey(q)){
+            TreeNode node=queue.poll();
+            if(node.left!=null){
+                parent.put(node.left,node);
+                queue.add(root.left);
+            }
+            if(node.right!=null){
+               parent.put(node.right,node);
+               queue.add(root.right);
+            }
+             
+        }
+        Set<TreeNode>set=new HashSet<>();
+        while(p!=null){
+            set.add(p);
+            p=parent.get(p);
+        }
+        while(!set.contains(q)){
+            q=parent.get(q);
+        }
+        return q;
+    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null)
             return null;
