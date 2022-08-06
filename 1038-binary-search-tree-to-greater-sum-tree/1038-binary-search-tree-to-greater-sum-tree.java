f735e47b45179a0929cbd64eee1490b756caf11e
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-06 07:28:32 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (34.09%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -14,18 +14,17 @@
  * }
  */
 class Solution {
-    int prev=0;
+    int sum=0;
+    public void solve(TreeNode root){
+        if(root==null){return ;}
+        solve(root.right);
+        sum+=root.val;
+        root.val=sum;
+        solve(root.left);
+    }
     public TreeNode bstToGst(TreeNode root) {
-        if(root==null){
-            return null;
-        }
-        
-        bstToGst(root.right);
-        prev+=root.val;
-        root.val=prev;
-        bstToGst(root.left);
-        return root;
-        
+      solve(root);
+      return root;
         
     }
 }
