084317899382281b34aa2e38ffafe08d0a996ffe
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-07 16:20:06 +0530

    Time: 20 ms (83.02%), Space: 73.7 MB (7.98%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -14,27 +14,16 @@
  * }
  */
 class Solution {
-    //Total TC=O(n1+n2)+O(n1)+O(n2)
-    public void sortedBSTArr(TreeNode root,Queue<Integer>list){
-        if(root==null){          
-            return ; 
-        }                             //O(n)
-        sortedBSTArr(root.left,list); //Inorder trabesal,gives sorted list of node values
-        list.offer(root.val);
-        sortedBSTArr(root.right,list);
+    List<Integer>list=new ArrayList<>();
+    public void inorder(TreeNode root){
+        if(root==null){return ;}
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
     }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        Queue<Integer> list1=new LinkedList<>();
-        sortedBSTArr(root1,list1);  //for first bst
-        Queue<Integer> list2=new LinkedList<>();
-        sortedBSTArr(root2,list2);//for second bst
-        List<Integer>ans=new ArrayList<>();
-        int i=0;int j=0;
-        while(!list1.isEmpty() || !list2.isEmpty()){
-          if(list1.isEmpty()){ans.add(list2.poll());}
-          else if(list2.isEmpty()){ans.add(list1.poll());}
-          else{ans.add(list1.peek()<list2.peek()?list1.poll():list2.poll());}
-        }
-        return ans;
+        inorder(root1);inorder(root2);
+        Collections.sort(list);  
+        return list;  
     }
 }
