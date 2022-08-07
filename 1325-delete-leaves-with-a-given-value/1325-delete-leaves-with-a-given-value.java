c25116275e9c4fd65504323cebef1291a46f0b23
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-07 18:11:44 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (53.75%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -15,20 +15,17 @@
  */
 class Solution {
    public TreeNode solve(TreeNode  root ,int target){
-      if(root == null)
-            return root;
-        
-       root.left = removeLeafNodes(root.left , target);
-        root.right = removeLeafNodes(root.right , target);
-        
-        if(root.left == null && root.right == null && root.val == target)
-            root = null;
-        
-        
+        if(root==null){
+            return null;
+        }
+        root.left=solve(root.left,target);
+        root.right=solve(root.right,target);
+        if(root.left==null && root.right==null && root.val==target){
+           return null;
+        }
         return root;
     }
     public TreeNode removeLeafNodes(TreeNode root, int target) {
-       
-        return  solve(root,target);
+        return solve(root,target);
     }
 }
