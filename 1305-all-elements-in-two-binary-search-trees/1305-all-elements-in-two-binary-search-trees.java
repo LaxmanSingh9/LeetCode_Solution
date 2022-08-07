79a6dd97658a514e19502e2b1e916a8a4eb0292f
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-07 16:59:51 +0530

    Time: 59 ms (11.78%), Space: 72.3 MB (35.74%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -15,15 +15,42 @@
  */
 class Solution {
     List<Integer>list=new ArrayList<>();
-    public void inorder(TreeNode root){
-        if(root==null){return ;}
-        inorder(root.left);
-        list.add(root.val);
-        inorder(root.right);
+    public void populate(TreeNode curr,Stack<TreeNode>stack){
+        while(curr!=null){
+            stack.push(curr);
+            curr=curr.left;
+         }
+    }
+    public List<Integer> solve(TreeNode root1,TreeNode root2){
+        Stack<TreeNode>stack1=new Stack<>();
+        Stack<TreeNode>stack2=new Stack<>();
+        List<Integer>ans=new ArrayList<>();
+        populate(root1,stack1);
+        populate(root2,stack2);
+        TreeNode curr=null;
+        while(!stack1.isEmpty() && !stack2.isEmpty()){
+           if(stack1.peek().val<stack2.peek().val){
+                TreeNode tempcurr=stack1.pop();
+                ans.add(tempcurr.val);
+                curr=tempcurr.right;
+                populate(curr,stack1);
+            }
+            else{
+                TreeNode tempcurr=stack2.pop();
+                ans.add(tempcurr.val);
+                curr=tempcurr.right;
+                populate(curr,stack2);
+            }
+        }
+        Stack<TreeNode>stack=stack1.isEmpty()?stack2:stack1;
+        while(!stack.isEmpty()){
+            curr=stack.pop();
+            ans.add(curr.val);
+            populate(curr.right,stack);
+        }
+        return ans;
     }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        inorder(root1);inorder(root2);
-        Collections.sort(list);  
-        return list;  
+       return solve(root1,root2);
     }
 }
