5059e3042dc39a398e4b23d5ebb18f72e22cc673
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-07 17:27:48 +0530

    Time: 0 ms (100.00%), Space: 54.3 MB (43.84%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -17,23 +17,12 @@ class Solution {
     //TC=O(N) left or right skwed tree
     //balance bst log(N)
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null)
-            return new TreeNode(val);
-        TreeNode parent=null;
-        TreeNode curr=root;
-        while(curr!=null){
-            parent=curr;
-            if(curr.val>val)
-              curr=curr.left;
-            else
-              curr=curr.right;
-        }
-        if(parent.val>val){
-            parent.left=  new TreeNode(val);
-        }
-        else{
-            parent.right= new TreeNode(val);
-        }
-        return root;
+       if(root==null)
+           return new TreeNode(val);
+       if(root.val>val)
+           root.left=insertIntoBST(root.left,val);
+       else
+           root.right=insertIntoBST(root.right,val);
+       return root;
     }
 }
