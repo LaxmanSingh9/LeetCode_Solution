bc4d9e12f29dc79b76bcae4d5493a884d26da1a1
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-20 13:01:15 +0530

    Time: 3 ms (14.16%), Space: 48.5 MB (10.55%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,14 +1,24 @@
 class Solution {
-    int [][]comToAdj=new int[][]{{0,1},{0,-1},{1,0},{-1,0},};
     public void solve(int [][]img,int st,int end,int color,int srcCol){
-        if(img[st][end]!=srcCol)
-            return;
-        img[st][end]=color;
-        for(int[]c:comToAdj){
-          if((st+c[0]>=0 && st+c[0]<img.length)&&(end+c[1]>=0 && end+c[1]<img[0].length))
-             solve(img,st+c[0],end+c[1],color,srcCol); 
-        }
+          int [][]comOfAdj=new int[][]{{0,1},{0,-1},{1,0},{-1,0},};
+          Queue<int[]>queue=new LinkedList<>();
+          queue.add(new int[]{st,end});
+          while(!queue.isEmpty()){
+              int []mat=queue.poll();
+              img[mat[0]][mat[1]]=color;
+              for(int []com:comOfAdj){
+                  int x=mat[0]+com[0],y=mat[1]+com[1];
+                  if((x>=0 && x<img.length) && (y>=0 && y<img[0].length) && img[x][y]==srcCol){
+                      queue.add(new int[]{x,y});
+                    
+                  }
+                     
+              } 
+          }
+          
     }
+    //TC:-m*n
+    //SC:-m*n
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         if(image[sr][sc]==color)
             return image;
