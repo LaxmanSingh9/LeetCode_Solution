0e6d47e8003b4fcc6fc8f04836f3fa01d72bd13a
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-20 18:26:04 +0530

    Time: 7 ms (32.36%), Space: 57.4 MB (55.91%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,19 +1,20 @@
 class Solution {
-    public void bfs(char [][]grid,int rowN,int colN){
-       int [][]comToAdj=new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    public void bfs(char [][]img,int rowN,int colN){
+       int [][]comOfAdj=new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
        Queue<int[]>queue=new LinkedList<>();
        queue.add(new int[]{rowN,colN});
-       grid[rowN][colN]='2';
+       img[rowN][colN]='2';
        while(!queue.isEmpty()){
-          int []mat=queue.poll();
-          for(int []com:comToAdj){
-            int adjRow=mat[0]+com[0],adjCol=mat[1]+com[1];
-            if((adjRow>=0 && adjRow<grid.length)&&(adjCol>=0 && adjCol<grid[0].length)&& grid[adjRow][adjCol]=='1'){
-                queue.add(new int[]{adjRow,adjCol});
-                grid[adjRow][adjCol]='2';
-            }
-        }
-      }
+           int []mat=queue.poll();
+           
+          for(int []com:comOfAdj){
+                  int x=mat[0]+com[0],y=mat[1]+com[1];
+                  if((x>=0 && x<img.length) && (y>=0 && y<img[0].length) && img[x][y]=='1'){
+                      queue.add(new int[]{x,y});
+                      img[x][y]='2';
+                  }
+          } 
+       }
     }
     public int numIslands(char[][] grid) {
         int count=0;
