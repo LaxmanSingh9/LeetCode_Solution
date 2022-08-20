6f39c70a1a14f263cbd2597cbc2f94acb679fe55
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-20 12:38:18 +0530

    Time: 2 ms (32.13%), Space: 48.1 MB (41.40%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,20 +1,18 @@
 class Solution {
     int [][]comToAdj=new int[][]{{0,1},{0,-1},{1,0},{-1,0},};
     public void solve(int [][]img,int st,int end,int color,int srcCol){
-        if(st<0 || st>=img.length|| end<0 || end>=img[0].length || img[st][end]!=srcCol)
-          return;   
+        if(img[st][end]!=srcCol)
+            return;
         img[st][end]=color;
-        for(int []com:comToAdj)
-          solve(img,st+com[0],end+com[1],color,srcCol);
+        for(int[]c:comToAdj){
+          if((st+c[0]>=0 && st+c[0]<img.length)&&(end+c[1]>=0 && end+c[1]<img[0].length))
+             solve(img,st+c[0],end+c[1],color,srcCol); 
+        }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         if(image[sr][sc]==color)
             return image;
         solve(image,sr,sc,color,image[sr][sc]);
         return image;
-        
-        //call an dfs
-        //call its all four direction , then so on(apply base condition also)
-        //take an visited array also
     }
 }
