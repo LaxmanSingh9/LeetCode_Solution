7e3deb4da2c7b7271d5966122fb683c5546be813
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-21 15:00:47 +0530

    Time: 3 ms (54.75%), Space: 51.2 MB (76.83%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -1,38 +1,34 @@
 class Solution {
     public void dfs(char[][]board,Integer rowN,Integer colN){
-        if((rowN<=0 || rowN>=board.length-1) || (colN<=0 || colN>=board[0].length-1) || board[rowN][colN]!='O')
+        if((rowN<0 || rowN>=board.length) || (colN<0 || colN>=board[0].length) || board[rowN][colN]!='O')
             return;
-        board[rowN][colN]='X';
+        board[rowN][colN]='*';
         dfs(board,rowN+1,colN);
         dfs(board,rowN-1,colN);
         dfs(board,rowN,colN+1);
         dfs(board,rowN,colN-1);
     }
-    public boolean isValidRegion(char[][]board,Integer rowN,Integer colN,int[][]vis){
-        if((rowN<=0 || rowN>=board.length-1 || colN<=0 || colN>=board[0].length-1) && board[rowN][colN]=='O'){
-           return false;
-        }
-           
-        if(rowN<0 || rowN>board.length || colN<0 || colN>board[0].length || board[rowN][colN]!='O')
-            return true;
-        if(vis[rowN][colN]==1){
-           return true; 
-        }
-           
-        vis[rowN][colN]=1;
-        boolean op1=isValidRegion(board,rowN+1,colN,vis);
-        boolean op2=isValidRegion(board,rowN-1,colN,vis);
-        boolean op3=isValidRegion(board,rowN,colN+1,vis);
-        boolean op4=isValidRegion(board,rowN,colN-1,vis);
-        boolean ans=op1&op2&op3&op4;
-        return ans;
-    }
     public void solve(char[][] board) {
-        int [][]visited=new int[board.length][board[0].length];
+        int row=board.length,col=board[0].length;
+        for(int i=0;i<col;i++){
+            if(board[0][i]=='O')
+                dfs(board,0,i);
+            if(board[row-1][i]=='O')
+               dfs(board,row-1,i);
+        }
+        for(int i=0;i<row;i++){
+            if(board[i][0]=='O')
+                dfs(board,i,0);
+            if(board[i][col-1]=='O')
+                dfs(board,i,col-1);
+        }
         for(int i=0;i<board.length;i+=1){
             for(int j=0;j<board[0].length;j+=1){
-               if(board[i][j]=='O' && visited[i][j]==0 && isValidRegion(board,i,j,visited)) 
-                    dfs(board,i,j);
+              if(board[i][j]=='O')
+                 board[i][j]='X';
+              if(board[i][j]=='*')
+                 board[i][j]='O';
+                  
             }
         }
     }
