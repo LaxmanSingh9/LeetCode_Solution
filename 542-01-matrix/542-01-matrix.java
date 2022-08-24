f1fc655f17d0ad4213ee19d32136fa2c513b6e5e
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-24 22:45:19 +0530

    Time: 26 ms (38.58%), Space: 73.1 MB (37.61%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,35 +1,32 @@
 class Solution {
-    int [][]comOfAdj=new int[][]{{0,1},{0,-1},{1,0},{-1,0},};
-    public void dfs(int[][]graph,int[][]dist,int u,int v,int prev){
-        dist[u][v]=prev;
-        for(int i=0;i<4;i+=1){
-            int x=u+comOfAdj[i][0],y=v+comOfAdj[i][1];
-            if((x>=0&&x<graph.length) && (y>=0 && y<graph[0].length)&& graph[x][y]!=0 && (dist[x][y]>prev+1)){
-                 dfs(graph,dist,x,y,prev+1);
-            }
-               
-        }
-    }
+    int [][]comOfAdj=new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
     public int[][] updateMatrix(int[][] mat) {
-         int m=mat.length,n=mat[0].length;
-         int [][]dist=new int[m][n];
-         for(int []ar:dist)
-             Arrays.fill(ar,Integer.MAX_VALUE);
-        
-         for(int i=0;i<m;i+=1){
-            for(int j=0;j<n;j+=1){
-                if(mat[i][j]==0){
-                    dfs(mat,dist,i,j,0);
+        int m=mat.length,n=mat[0].length;
+        int[][]dist=new int[m][n];
+        Queue<int[]>queue=new LinkedList<>();
+        for(int[]ar:dist)
+            Arrays.fill(ar,Integer.MAX_VALUE);
+        for(int u=0;u<m;u++){
+          for(int v=0;v<n;v++){
+              if(mat[u][v]==0){
+                   dist[u][v]=0;
+                   queue.add(new int[]{u,v});
+              }
+          }
+        }
+        while(!queue.isEmpty()){
+            int []node=queue.poll();
+            int u=node[0],v=node[1],x=0,y=0;
+            for(int i=0;i<4;i++){
+                x=u+comOfAdj[i][0];
+                y=v+comOfAdj[i][1];
+                if((x>=0 && x<m) && (y>=0 && y<n) && mat[x][y]!=0 && dist[x][y]>dist[u][v]+1){
+                    dist[x][y]=dist[u][v]+1;
+                    queue.add(new int[]{x,y});
                 }
             }
-         }
-    
-         return dist;
-        
-        
-        
-        //Run an dfs or bfs for all the zeros and keep update the distance
-        
-        
+            
+        }
+        return dist;
     }
 }
