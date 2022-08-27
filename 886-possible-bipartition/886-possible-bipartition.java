80e20b1e88b6e1be99e4a3032713f6e8fb75088b
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-27 10:25:05 +0530

    Time: 16 ms (91.35%), Space: 72.7 MB (54.30%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.java
+++ 886-possible-bipartition/886-possible-bipartition.java
@@ -1,4 +1,7 @@
 class Solution {
+    
+    //TC=>O(V+E)
+    //SC+>O(V+E)
     public void addEdge(ArrayList<ArrayList<Integer>>adj,int u,int v){
         adj.get(u-1).add(v-1);
         adj.get(v-1).add(u-1);
@@ -6,7 +9,7 @@ class Solution {
     public boolean checkBiPartitions(ArrayList<ArrayList<Integer>>graph,int []colorArr,int u,int col){
         colorArr[u]=col;
         for(int v:graph.get(u)){
-            if(colorArr[v]==-1 && !checkBiPartitions(graph,colorArr,v,col^1))
+            if(colorArr[v]==0 && !checkBiPartitions(graph,colorArr,v,-col))
                 return false;
             else if(colorArr[u]==colorArr[v])
                 return false;
@@ -18,13 +21,12 @@ class Solution {
             return true;
         ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
         int []colorArr=new int[n];
-        Arrays.fill(colorArr,-1);
         for(int i=0;i<n;i+=1)
             adj.add(new ArrayList<>());
         for(int i=0;i<dislikes.length;i++)
             addEdge(adj,dislikes[i][0],dislikes[i][1]);
         for(int i=0;i<n;i+=1){
-            if(colorArr[i]==-1 && !checkBiPartitions(adj,colorArr,i,1))
+            if(colorArr[i]==0 && !checkBiPartitions(adj,colorArr,i,1))
                 return false;
         }
         return true;
