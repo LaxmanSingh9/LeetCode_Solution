2923875cbaccb3b1c78aee571ec013e031422973
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-27 21:27:45 +0530

    Time: 5 ms (85.51%), Space: 48 MB (55.23%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,40 +1,29 @@
 class Solution {
-    public boolean hasCycle(Map<Integer,List<Integer>>graph,int s,int[]vis,int []rest){
-        if(vis[s]==1){
-            return rest[s]==1;
+    public boolean canFinish(int n, int[][] prerequisites) {        
+        int []incomingEdges=new int[n];
+        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            adj.add(new ArrayList<Integer>());
         }
-        vis[s]=1;
-        rest[s]=1;
-        for(int e:graph.get(s)){
-            if(hasCycle(graph,e,vis,rest)){
-                return true;
+        for(int []pre:prerequisites){
+            incomingEdges[pre[1]]++;
+            adj.get(pre[0]).add(pre[1]);
+        }
+        Queue<Integer>queue=new LinkedList<>();
+        for(int v=0;v<n;v+=1){
+            if(incomingEdges[v]==0)
+                queue.add(v);
+        }
+       
+        while(!queue.isEmpty()){
+            int u=queue.poll();
+            for(int v:adj.get(u)){
+                if(--incomingEdges[v]==0){
+                    queue.add(v);
+                }
             }
+            n-=1;
         }
-        rest[s]=0;
-        return false;
-        
-        
-        
-        
-    }
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-         int n=numCourses;
-        Map<Integer,List<Integer>> graph = new HashMap();
-        for(int i = 0 ; i < n ; i++){
-            graph.put(i, new ArrayList());
-        }
-        int []inDeg=new int[n];
-        for(int edges[]:prerequisites){
-            graph.get(edges[1]).add(edges[0]);
-            
-        }
-        int []visited=new int[n];
-        int []rest=new int[n];
-        for(int i=0;i<n;i+=1){
-            if(visited[i]!=1 && hasCycle(graph,i,visited,rest)){
-                return false;
-            }
-        }
-        return true;
-    }
+        return n==0;
+     }
 }
