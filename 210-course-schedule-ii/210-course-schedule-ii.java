db37b1b845ebae38a55aa6412586481cf49c0025
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-27 23:06:55 +0530

    Time: 7 ms (77.47%), Space: 48 MB (80.37%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,34 +1,43 @@
 class Solution {
+    public boolean doesContainsCycle(ArrayList<ArrayList<Integer>>adjList,boolean[]vis,int[]recSt,int u,Stack<Integer>st){
+        vis[u]=true;
+        recSt[u]=1;
+        for(int v:adjList.get(u)){
+            if(!vis[v] &&  doesContainsCycle(adjList,vis,recSt,v,st))
+                return true;
+            else if(recSt[v]==1)
+                return true;
+            
+        }
+        recSt[u]=2;
+        st.push(u);
+        return false;
+    
+    }
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>>adjList=new ArrayList<>();
         for(int i=0;i<numCourses;i+=1)
             adjList.add(new ArrayList<>());
-        int[]inorder=new int[numCourses];
-        for(int []pre:prerequisites){
-            inorder[pre[0]]+=1;
+        for(int []pre:prerequisites)
             adjList.get(pre[1]).add(pre[0]);
+        boolean vis[]=new boolean[numCourses];
+        int recSt[]=new int[numCourses];
+        Stack<Integer>preqStack=new Stack<>();
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i] && doesContainsCycle(adjList,vis,recSt,i,preqStack))
+                return new int[0];
+            else if(vis[i] && recSt[i]==1)
+                return new int[0];
         }
-        Queue<Integer>queue=new LinkedList<>();
-        List<Integer>ans=new ArrayList<>();
-        for(int v=0;v<numCourses;v++){
-            if(inorder[v]==0){
-                ans.add(v);
-                queue.add(v);
-            }
+        int []ansArr=new int[preqStack.size()];
+        int idx=0;
+        while(!preqStack.empty()){
+            int top=preqStack.peek();
+            preqStack.pop();
+            ansArr[idx++]=top;
         }
-        while(!queue.isEmpty()){
-            int u=queue.poll();
-            for(int v:adjList.get(u)){
-                --inorder[v];
-                if(inorder[v]==0){
-                   ans.add(v);
-                   queue.add(v);
-                }
-            }
-        }
-        int []ansArr=new int[ans.size()];
-        for(int i=0;i<ans.size();i++)
-            ansArr[i]=ans.get(i);
-        return ans.size()!=numCourses?new int[0]:ansArr;
+        return ansArr;
+        
+       
     }
 }
