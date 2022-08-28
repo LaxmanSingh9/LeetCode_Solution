51d8a64f137833bf6d71eed12f6d21bbeb48bd0d
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-28 17:28:26 +0530

    Time: 213 ms (5.13%), Space: 50.2 MB (9.79%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,38 +1,32 @@
 class Solution {
-    public boolean dfs(Map<Integer,List<Integer>>graph,int[]visited,int s,int d){
-        if(s==d){
-            return true;
-        }
-        visited[s]=1;
-        for(int edge:graph.get(s)){
-            if(visited[edge]!=1){
-                if(dfs(graph,visited,edge,d))
-                    return true;
-            }
+    public boolean hasCycle(int [][]graph,boolean[]vis,int u,int parent){
+        vis[u]=true;
+        for(int i=0;i<graph.length;i++){
+           if(graph[u][i]==1){
+              if(vis[i]==false){
+                  if(hasCycle(graph,vis,i,u))
+                     return true;
+              }                
+              else if(i!=parent){
+                 return true;
+              }   
+            } 
         }
         return false;
     }
     public int[] findRedundantConnection(int[][] edges) {
-        Map<Integer,List<Integer>> graph = new HashMap();
         int n=edges.length;
-        
-        for(int i = 0 ; i < n ; i++){
-            graph.put(i, new ArrayList());
-        }
-         //construct graph, add bidirectional vertex
-        for(int[] edge : edges){
-           boolean  check=dfs(graph,new int[n],edge[0]-1,edge[1]-1);
-               if(check==true){
-                   return edge;
-               }
-                   
-               
+        int [][]graph=new int[n][n];
+        boolean []vis;
+        for(int []edge:edges){
+            vis=new boolean[n];
+            graph[edge[0]-1][edge[1]-1]=1;
+            graph[edge[1]-1][edge[0]-1]=1;
+            if(hasCycle(graph,vis,edge[0]-1,edge[1]-1)){
+               return edge;  
+            }
            
-           graph.get(edge[0]-1).add(edge[1]-1);
-           graph.get(edge[1]-1).add(edge[0]-1);
         }
-		//start dfs from start point
-       throw new AssertionError();
-        
+        return new int[0];
     }
 }
