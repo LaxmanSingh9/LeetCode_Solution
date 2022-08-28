efff26f410ee435cb78c983d3a74e8c34add08c3
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-28 22:23:25 +0530

    Time: 70 ms (33.32%), Space: 49.9 MB (56.08%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
@@ -1,27 +1,24 @@
 class Solution {
-    // Ans = # of stones – # of islands
-    public int removeStones(int[][] stones) {
-        Set<int[]> visited = new HashSet();
-        int numOfIslands = 0;
-        for (int[] s1:stones){
-            
-            if (!visited.contains(s1)){
-               dfs(s1, visited, stones); 
-               numOfIslands++;
+    Set<int[]> visited=new HashSet<>();
+    public void dfs(int[][]arr,int []stone){
+        visited.add(stone);
+        for(int []ar:arr){
+            int newU=ar[0],newV=ar[1];
+            if((stone[0]==newU || stone[1]==newV) && visited.contains(ar)==false) {
+                dfs(arr,ar);
             }
         }
-        
-        return stones.length - numOfIslands;
     }
-    
-    private void dfs(int[] s1, Set<int[]> visited, int[][] stones){
-        visited.add(s1);
-        for (int[] s2: stones){
-            if (!visited.contains(s2)){
-				// stone with same row or column. group them into island
-                if (s1[0] == s2[0] || s1[1] == s2[1])
-                    dfs(s2, visited, stones);
-            }
-        }
+    public int removeStones(int[][] stones) {
+      int remainStones=0;
+      for(int []stone:stones){
+          int x=stone[0],y=stone[1];
+          if(visited.contains(stone)==false){
+             dfs(stones,stone);
+             remainStones+=1;
+          }
+      }
+      return stones.length-remainStones; 
+         
     }
 }
