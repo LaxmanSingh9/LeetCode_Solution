9ca2aa61f8340170b891e141936f50b4b4720520
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-28 10:32:24 +0530

    Time: 1 ms (99.99%), Space: 54.5 MB (24.03%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,61 +1,21 @@
-class UnionFind {
-    private int count;
-    private int[] parent,rank;
-    public UnionFind(int n) {
-      parent = new int[n];
-      rank = new int[n];
-      count=n;
-      for(int i = 0; i < n; i++) {
-        parent[i] = i;
-      }
-    }
-    public void union(int x, int y) {
-      int rootX = find(x);
-      int rootY = find(y);
-      
-      if(rootX != rootY) {
-        if(rank[rootX] > rank[rootY]) {
-          rank[rootX]++;
-          parent[rootY] = rootX;
-        } else {
-          rank[rootY]++;
-          parent[rootX] = rootY;
-        }
-        count--;
-      }
-    }
-    
-    public int find(int x) {
-      if(parent[x] == x) return x;
-      
-      int root = find(parent[x]);
-      parent[x] = root; //path compression
-      return root;
-    }
-    
-    public boolean isConnected(int x, int y) {
-      return find(x) == find(y);
-    }
-    public int getCount(){
-        return count;
-    }
-  }
-
-
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        UnionFind ufind=new UnionFind(n);
-        for(int i=0;i<n;i++){
-          for(int j=0;j<i;j++){
-            if(isConnected[i][j]==1){
-                ufind.union(i,j);
-                 
-              }
-           }
+    public void connectedProvinces(int[][]graph,boolean[]vis,int u){
+        vis[u]=true;
+        for(int i=0;i<graph.length;i++){
+            if(!vis[i] && graph[u][i]==1){
+                connectedProvinces(graph,vis,i);
+            }
         }
-        
-
-        return ufind.getCount();
+    }
+    public int findCircleNum(int[][] isConnected){
+        int provinces=0,n=isConnected.length;
+        boolean[] vis=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                connectedProvinces(isConnected,vis,i);
+                provinces++;
+            }
+        }
+        return provinces;
     }
 }
