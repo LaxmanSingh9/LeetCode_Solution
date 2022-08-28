c71460b634a4f98c1fad95eff2e1c9bcd5fdc1f8
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-28 10:39:06 +0530

    Time: 8 ms (18.09%), Space: 54.4 MB (24.03%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,11 +1,17 @@
 class Solution {
     public void connectedProvinces(int[][]graph,boolean[]vis,int u){
-        vis[u]=true;
-        for(int i=0;i<graph.length;i++){
-            if(!vis[i] && graph[u][i]==1){
-                connectedProvinces(graph,vis,i);
-            }
-        }
+       Queue<Integer>q=new LinkedList<>();
+       q.add(u);
+       while(!q.isEmpty()){
+           int ver=q.poll();
+           vis[ver]=true;
+           for(int i=0;i<graph.length;i++){
+               if(!vis[i] && graph[ver][i]==1){
+                    q.add(i);
+               }
+                  
+           }
+       }
     }
     public int findCircleNum(int[][] isConnected){
         int provinces=0,n=isConnected.length;
