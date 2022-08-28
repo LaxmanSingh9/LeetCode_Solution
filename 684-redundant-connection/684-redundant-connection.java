74fc4932125975d0426aea3ac56fbde2ab810f8c
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-28 20:59:52 +0530

    Time: 1 ms (89.71%), Space: 42.6 MB (90.35%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,32 +1,63 @@
-class Solution {
-    public boolean hasCycle(int [][]graph,boolean[]vis,int u,int parent){
-        vis[u]=true;
-        for(int i=0;i<graph.length;i++){
-           if(graph[u][i]==1){
-              if(vis[i]==false){
-                  if(hasCycle(graph,vis,i,u))
-                     return true;
-              }                
-              else if(i!=parent){
-                 return true;
-              }   
-            } 
-        }
-        return false;
+class UnionFind {
+    private int count;
+    private int[] parent,rank;
+    public UnionFind(int n) {
+      parent = new int[n];
+      rank = new int[n];
+      count=n;
+      for(int i = 0; i < n; i++) {
+        parent[i] = i;
+      }
     }
-    public int[] findRedundantConnection(int[][] edges) {
-        int n=edges.length;
-        int [][]graph=new int[n][n];
-        boolean []vis;
-        for(int []edge:edges){
-            vis=new boolean[n];
-            graph[edge[0]-1][edge[1]-1]=1;
-            graph[edge[1]-1][edge[0]-1]=1;
-            if(hasCycle(graph,vis,edge[0]-1,-1)){
-               return edge;  
-            }
-           
+    public void union(int x, int y) {
+      int rootX = find(x);
+      int rootY = find(y);
+      
+      if(rootX != rootY) {
+        if(rank[rootX] > rank[rootY]) {
+          rank[rootX]++;
+          parent[rootY] = rootX;
+        } else {
+          rank[rootY]++;
+          parent[rootX] = rootY;
         }
-        return new int[0];
+        count--;
+      }
+    }
+    
+    public int find(int x) {
+      if(parent[x] == x) return x;
+      
+      int root = find(parent[x]);
+      parent[x] = root; //path compression
+      return root;
+    }
+    
+    public boolean isConnected(int x, int y) {
+      return find(x) == find(y);
+    }
+    public int getCount(){
+        return count;
+    }
+  }
+
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+         int n=edges.length;
+         UnionFind uf=new UnionFind(n);
+         for(int[]edge:edges){
+             boolean flag=uf.isConnected(edge[0]-1,edge[1]-1);
+             if(flag)
+                 return edge;
+             uf.union(edge[0]-1,edge[1]-1);
+         }
+         return new int[0];
     }
 }
+
+
+
+
+
+
+
