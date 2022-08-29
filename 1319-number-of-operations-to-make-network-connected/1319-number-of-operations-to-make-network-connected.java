3e4d043ea8ac7bb25c1b74fba61b9cb69ec2e6e0
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-08-29 10:24:27 +0530

    Time: 5 ms (91.98%), Space: 64.4 MB (64.15%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,3 +1,47 @@
+class UnionFind {
+    private int count;
+    private int[] parent,rank;
+    public UnionFind(int n) {
+      parent = new int[n];
+      rank = new int[n];
+      count=n;
+      for(int i = 0; i < n; i++) {
+        parent[i] = i;
+      }
+    }
+    public void union(int x, int y) {
+      int rootX = find(x);
+      int rootY = find(y);
+      
+      if(rootX != rootY) {
+        if(rank[rootX] > rank[rootY]) {
+          rank[rootX]++;
+          parent[rootY] = rootX;
+        } else {
+          rank[rootY]++;
+          parent[rootX] = rootY;
+        }
+        count--;
+      }
+    }
+    
+    public int find(int x) {
+      if(parent[x] == x) return x;
+      
+      int root = find(parent[x]);
+      parent[x] = root; //path compression
+      return root;
+    }
+    
+    public boolean isConnected(int x, int y) {
+      return find(x) == find(y);
+    }
+    public int getCount(){
+        return count;
+    }
+ }
+
+
 class Solution {
     //To connected n nodes , we al least need n-1 unique edges
     //if there are n-1 edges then minimum opeation will be total disconnected componets in graph
@@ -10,23 +54,11 @@ class Solution {
     }
     public int makeConnected(int n, int[][] connections) {
         int totalConnections=connections.length;
-        if(totalConnections<n-1)
+        if(totalConnections<n-1)//Most important , there are less then n-1 edges then we can not make connect a graph
             return -1;
-        ArrayList<ArrayList<Integer>>adjList=new ArrayList<>();
-        for(int i=0;i<n;i++)
-            adjList.add(new ArrayList<>());
-        for(int []con:connections){
-            adjList.get(con[0]).add(con[1]);
-            adjList.get(con[1]).add(con[0]);
-        }
-        boolean[]vis=new boolean[n];
-        int totalComponents=0;
-        for(int i=0;i<n;i+=1){
-            if(!vis[i]){
-                dfs(adjList,vis,i);
-                totalComponents+=1;
-            }
-        }
-        return totalComponents-1;//TotalDisconnectedComponents-1
+        UnionFind uf=new UnionFind(n);
+        for(int []con:connections)
+            uf.union(con[0],con[1]);
+        return uf.getCount()-1; //TotalDisconnectedComponents-1
     }
 }
