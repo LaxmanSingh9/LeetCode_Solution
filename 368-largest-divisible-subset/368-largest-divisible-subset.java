3b09f512dfa512fa7b09fc0ae005a3a93a33d7ca
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-01 10:02:36 +0530

    Time: 24 ms (86.97%), Space: 43.5 MB (67.42%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.java
+++ 368-largest-divisible-subset/368-largest-divisible-subset.java
@@ -1,54 +1,28 @@
 class Solution {
-    private List<Integer>getOrderedLIS(List<Integer>listOfMaxLIS,Map<Integer,Integer>map){
-       ArrayList<ArrayList<Integer>>arList=new ArrayList<>();
-       for(int val:listOfMaxLIS){
-           ArrayList<Integer>ar=new ArrayList<>();
-           ar.add(val);ar.add(map.get(val));
-           arList.add(ar);
-       }
-       Collections.sort(arList,(l1, l2) -> {
-         if(l1.get(0) > l2.get(0))
-            return 1;
-         else if(l1.get(0) < l2.get(0))
-           return -1;
-         else
-           return 0;
-       });
-       List<Integer>orderedLIS=new ArrayList<>();
-       for(List<Integer>ar:arList){
-           orderedLIS.add(ar.get(0));
-       } 
-       return orderedLIS;
-    }
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int n=nums.length,ans=0,indxWOfMaxLIS=0;
-        Map<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<n;i++){
-             map.put(nums[i],i);    
-        }
+        int n=nums.length,maxLis=0,index=-1;
+        int []lis=new int[n],prev=new int[n];
         Arrays.sort(nums);
-        ArrayList<ArrayList<Integer>>listOfAllSub=new ArrayList<>();
         for(int i=0;i<n;i++){
-            listOfAllSub.add(new ArrayList<>());
-            //listOfAllSub.get(i).add(nums[i]);
-        }
-        
-        for(int i=0;i<n;i++){
-          ArrayList<Integer>ar=new ArrayList<>(listOfAllSub.get(i));
-          for(int j=i-1;j>=0;j--){
-              if(nums[i]%nums[j]==0 && ar.size()<=listOfAllSub.get(j).size()){
-                  ar=new ArrayList<>(listOfAllSub.get(j));
+           lis[i]=1;
+           prev[i]=-1; 
+           for(int j=i-1;j>=0;j--){
+               if(nums[i]%nums[j]==0 && lis[i]<lis[j]+1){
+                   lis[i]=lis[j]+1;
+                   prev[i]=j;
                }
-          }
-       
-          ar.add(nums[i]);  
-          listOfAllSub.get(i).addAll(ar);
-          if(listOfAllSub.get(i).size()>ans){
-             ans=listOfAllSub.get(i).size();
-             indxWOfMaxLIS=i; 
-          }
+           }
+           if(lis[i]>maxLis){
+               maxLis=lis[i];
+               index=i;
+           }
        }
-       
-       return getOrderedLIS(listOfAllSub.get(indxWOfMaxLIS),map);
+       ArrayList<Integer>res=new ArrayList<>(); 
+       while(index!=-1){
+          res.add(nums[index]);
+          index=prev[index];
+       } 
+       return res; 
+        
     }
 }
