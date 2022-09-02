17ecacdf3cc50a9884d31ae5aea39468c8735e5e
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-02 21:44:52 +0530

    Time: 8 ms (68.50%), Space: 49.2 MB (24.18%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,25 +1,13 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
         int []freq=new int[10003];
-        List<Integer>arr=new ArrayList<>();
-        arr.add(0);
-        for(int val:nums){
-             if(freq[val]==0)
-                arr.add(val); 
-             freq[val]+=1;
-        }
-        Collections.sort(arr);
-        int []dp=new int[arr.size()];
-        dp[1]=freq[arr.get(1)]*arr.get(1);
-        for(int i=2;i<arr.size();i+=1){
-            int curVal=freq[arr.get(i)]*arr.get(i);
-            if(arr.get(i)-arr.get(i-1)!=1){
-                dp[i]=dp[i-1]+curVal;
-            }
-            else{
-                dp[i]=Math.max(dp[i-1],curVal+dp[i-2]);
-            }
-        }
+        for(int val:nums)
+            freq[val]+=1;
+        int []dp=new int[10003];
+        dp[1]=freq[1];
+        for(int i=2;i<freq.length;i+=1){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+(freq[i]*i));
+         }
         return dp[dp.length-1];
     }
 }
