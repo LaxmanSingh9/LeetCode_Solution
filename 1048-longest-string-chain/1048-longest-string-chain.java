f167841bd33439f6afacf0143588da877362868a
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-03 20:20:35 +0530

    Time: 87 ms (32.94%), Space: 67.3 MB (29.69%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,28 +1,17 @@
 class Solution {
-    public boolean isPredecessor(String s1,String s2){
-        if(s1.length()-s2.length()!=1)
-            return false;
-        for(int i=0;i<s1.length();i++){
-            StringBuilder sb=new StringBuilder(s1);
-            if(sb.deleteCharAt(i).toString().equals(s2))
-                return true;
-        }
-        return false;
-    }    
-    public int longestStrChain(String[] words) {
+   public int longestStrChain(String[] words) {
         int n=words.length,longestChain=0;
-        int []dp=new int[n];
-        Arrays.sort(words,(s1,s2)->{
-            return s1.length()-s2.length();});
-        for(int i=0;i<n;i++){
-           dp[i]=1; 
-           for(int j=i-1;j>=0;j--){
-               if(words[i].length()-words[j].length()>1)
-                    break;
-               if(isPredecessor(words[i],words[j]))
-                   dp[i]=Math.max(dp[i],dp[j]+1);
+        Arrays.sort(words,(s1,s2)->{return s1.length()-s2.length();});
+        Map<String,Integer>map=new HashMap<>();
+        for(String str:words){
+           map.put(str,1); 
+           for(int i=0;i<str.length();i++){
+            StringBuilder sb=new StringBuilder(str);
+            String oneCharDiffStr=sb.deleteCharAt(i).toString();
+            if(map.containsKey(oneCharDiffStr) && map.get(oneCharDiffStr)+1>map.get(str))   
+                map.put(str,map.get(oneCharDiffStr)+1);
            }
-           longestChain=Math.max(longestChain,dp[i]);
+           longestChain=Math.max(longestChain,map.get(str));
         }
         return longestChain;
     }
