1620e6e54bb5e1cc80376287bd5120708f0e8655
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-04 12:08:27 +0530

    Time: 108 ms (31.49%), Space: 54.3 MB (52.12%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -6,9 +6,10 @@ class Solution {
           for(int i=1;i<n+1;i++){
              dp[i][0]=true; 
              for(int j=1;j<target+1;j++){
-                 if(j>=nums[i-1])  //include
+                 if(j>=nums[i-1])  //include & skip
                      dp[i][j]=dp[i-1][j]||dp[i-1][j-nums[i-1]];
-                 dp[i][j]=dp[i][j]||dp[i-1][j];//skip 
+                 else
+                    dp[i][j]=dp[i-1][j];//skip 
              } 
           }
           return dp[n][target];
