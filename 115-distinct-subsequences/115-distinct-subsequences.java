6ef3978aa340b535ab043b56d10e3a5845c54b9c
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-08 08:38:12 +0530

    Time: 57 ms (5.76%), Space: 50.3 MB (47.16%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,14 +1,13 @@
 class Solution {
-    public int lcs(String text1 ,String text2,int i,int j,int len,int [][]memo){
-        if(i>=text1.length() || j>=text2.length())
-            return text2.length()==len?1:0;
+    public int lcs(String text1 ,String text2,int i,int j,int [][]memo){
+        if(j>=text2.length() || i>=text1.length())
+           return text2.length()==j?1:0;
         if(memo[i][j]!=-1)
             return memo[i][j];
         int a=0,b=0,c=0;
-        if(text1.charAt(i)==text2.charAt(j)){
-            a=lcs(text1,text2,i+1,j+1,len+1,memo);
-        }
-        b=lcs(text1,text2,i+1,j,len,memo);    
+        if(text1.charAt(i)==text2.charAt(j))
+            a=lcs(text1,text2,i+1,j+1,memo);
+        b=lcs(text1,text2,i+1,j,memo);    
         memo[i][j]=a+b;
         return memo[i][j];
     }
@@ -17,7 +16,6 @@ class Solution {
          for(int []ar:memo){
             Arrays.fill(ar,-1);  
          } 
-        
-        return lcs(s,t,0,0,0,memo);
+         return lcs(s,t,0,0,memo);
     }
 }
