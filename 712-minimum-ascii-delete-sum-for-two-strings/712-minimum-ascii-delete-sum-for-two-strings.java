5afd6c08d0ac07bba09ba7a56faf4be9856dd806
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-08 10:14:19 +0530

    Time: 66 ms (14.06%), Space: 54.5 MB (6.83%) - LeetHub

--- 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.java
@@ -7,16 +7,18 @@ class Solution {
         return asciiSum;
     }
     public int getMaxAsciiSumOfLCS(String s1,String s2,int i,int j,int[][]memo){
-        if(i>=s1.length() || j>=s2.length() || memo[i][j]!=-1)
-            return memo[i][j]==-1?0:memo[i][j];
+        if(i>=s1.length() || j>=s2.length())
+            return i>=s1.length()?totalAsciiSum(s2.substring(j)):totalAsciiSum(s1.substring(i));
+        if(memo[i][j]!=-1)
+            return memo[i][j];
         int incl=0,skip=0;
         if(s1.charAt(i)==s2.charAt(j)){
-            incl=s1.charAt(i)+getMaxAsciiSumOfLCS(s1,s2,i+1,j+1,memo);
+            incl=getMaxAsciiSumOfLCS(s1,s2,i+1,j+1,memo);
             memo[i][j]=incl;
         }
         else{
-            memo[i][j]=Math.max(getMaxAsciiSumOfLCS(s1,s2,i+1,j,memo),
-                      getMaxAsciiSumOfLCS(s1,s2,i,j+1,memo));
+            memo[i][j]=Math.min(s1.charAt(i)+getMaxAsciiSumOfLCS(s1,s2,i+1,j,memo),
+                      s2.charAt(j)+getMaxAsciiSumOfLCS(s1,s2,i,j+1,memo));
         }  
         return memo[i][j];
     }
@@ -24,6 +26,6 @@ class Solution {
         int [][]memo=new int[s1.length()+1][s2.length()+1];
         for(int []ar:memo)
             Arrays.fill(ar,-1);
-        return totalAsciiSum(s1)+totalAsciiSum(s2)-2*getMaxAsciiSumOfLCS(s1,s2,0,0,memo);
+        return getMaxAsciiSumOfLCS(s1,s2,0,0,memo);
     }
 }
