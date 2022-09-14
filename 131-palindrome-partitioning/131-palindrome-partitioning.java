f412511b34378611f0090a62bf935dc5015dd39d
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-14 09:56:52 +0530

    Time: 21 ms (48.71%), Space: 194 MB (42.14%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,32 +1,32 @@
 class Solution {
-    List<List<String>>ans=new ArrayList<>();
-    public String isPalm(String s,int l ,int r){
-        int l1=l,r1=r;
-        while(l<r && s.charAt(l)==s.charAt(r)){
-            l+=1;
-            r-=1;
+    public String isPalm(String s,int l,int r){
+       int i=l,j=r-1; 
+       while(i<=j){
+           if(s.charAt(i)!=s.charAt(j)){
+               return "";
+           }
+           i+=1;j-=1;
        }
-       return l>=r?s.substring(l1,r1+1):"";
+       return s.substring(l,r); 
     }
-    public void solve(String s ,int st ,int e ,List<String>list,boolean [][]memo){
-        if(e-st==0 ){
+    public List<List<String>>ans=new ArrayList<>();
+    public void solve(String s,int st,int e,List<String>list){
+        //base case
+        if(e-st==0){
             ans.add(new ArrayList<>(list));
-            return ;
         }
-        for(int i=st;i<e;i+=1){
-           String str=isPalm(s,st,i);
-           if(str.length()>0){
-                list.add(str);
-                solve(s,i+1,e,list,memo);
+        for(int i=st;i<e;i++){
+            String pal=isPalm(s,st,i+1);
+           //System.out.println(pal);
+            if(pal.length()>0){
+                list.add(pal);
+                solve(s,i+1,e,list);
                 list.remove(list.size()-1);
             }
         }
-        return;
-       
     }
     public List<List<String>> partition(String s) {
-        int n=s.length();boolean[][]memo=new boolean[n+1][n+1];
-        solve(s ,0,n,new ArrayList<>(),memo);
+        solve(s,0,s.length(),new ArrayList<>());
         return ans;
     }
 }
