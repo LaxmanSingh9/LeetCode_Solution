6470c64ab1cbf8fd85345218a80633b936e1406d
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-15 22:18:28 +0530

    Time: 1670 ms (5.01%), Space: 44.4 MB (27.93%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,15 +1,32 @@
 class Solution {
-    public int countSubstrings(String s) {
-       boolean [][]dp=new boolean[s.length()][s.length()];
-       int ans=0,n=s.length();
-       for(int i=0;i<n;i++){
-          for(int j=0;j<=i;j++){
-              if(s.charAt(i)==s.charAt(j) && (i-j<=2 || dp[j+1][i-1])){
-                  dp[j][i]=true;
-                  ans+=1;
+    public boolean isPalm(String s, int l,int r){
+        while(l<r){
+            if(s.charAt(l++)!=s.charAt(r--))
+                return false;
+        }
+        return true;
+    }
+    public void solve(String s,int st,int e,boolean[][] dp){
+          if(st==e){
+              dp[st][e]=true;
+              return ;
+          }
+          for(int i=st;i<=e;i++){
+             if(!dp[st][i] && isPalm(s,st,i)){
+                 dp[st][i]=true;
+                 solve(s,i+1,e,dp);
              }
           } 
-       } 
+    }
+    public int countSubstrings(String s) {
+       boolean [][]dp=new boolean[s.length()+1][s.length()+1];
+       int ans=0,n=s.length();
+       solve(s,0,s.length()-1,dp);
+       for(int i=0;i<n;i++){
+          for(int j=0;j<n;j++){
+              if(dp[i][j])ans++;
+          } 
+       }
        return ans; 
-   }
+    }
 }
