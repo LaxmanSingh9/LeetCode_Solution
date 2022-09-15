808ffba7ce8103e0df789364a02e103c79636dd2
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-15 08:54:38 +0530

    Time: 20 ms (54.68%), Space: 196.2 MB (25.11%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,26 +1,28 @@
 class Solution {
-    public boolean isPalm(String s,int l,int r){
-       while(l<r){
-           if(s.charAt(l++)!=s.charAt(r--))return false;
-       }
-       return true;
-    }
     public List<List<String>>ans=new ArrayList<>();
-    public void solve(String s,int st,int e,List<String>list){
+    public void solve(String s,int st,int e,List<String>list,boolean [][]dp){
         //base case
         if(e-st==0){
             ans.add(new ArrayList<>(list));
         }
         for(int i=st;i<e;i++){
-            if(isPalm(s,st,i)) {
+            if(dp[st][i]) {
                 list.add(s.substring(st,i+1));
-                solve(s,i+1,e,list);
+                solve(s,i+1,e,list,dp);
                 list.remove(list.size()-1);
             }
         }
     }
     public List<List<String>> partition(String s) {
-        solve(s,0,s.length(),new ArrayList<>());
+        boolean dp[][]=new boolean[s.length()+1][s.length()+1];
+         for(int i=0;i<s.length();i++){//end
+           for(int j=0;j<=i;j++){//start
+               if(s.charAt(i)==s.charAt(j) && (i-j<=2 || dp[j+1][i-1])) {
+                   dp[j][i]=true;
+               }
+           } 
+        }
+        solve(s,0,s.length(),new ArrayList<>(),dp);
         return ans;
     }
 }
