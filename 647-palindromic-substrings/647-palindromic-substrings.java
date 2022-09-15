fdb1e36222a1131626256ccc3aa36ee2c0136cb7
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-15 21:08:54 +0530

    Time: 20 ms (34.01%), Space: 44.4 MB (29.80%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,37 +1,15 @@
-import java.util.*;
 class Solution {
-     public int solve(int l,int r,char[] s,int [][]memo){
-        if(r<l){// if lenght is 1
-            return 1;
-        }
-        if(memo[l][r]!=-1){
-            return memo[l][r];
-        }
-        memo[l][r]=s[l]!=s[r]?0:solve(l+1,r-1,s,memo);
-        solve(l+1,r,s,memo);
-        solve(l,r-1,s,memo);
-        return memo[l][r];
-        
-    }
-    
-    
     public int countSubstrings(String s) {
-      int n=s.length();
-        int [][]memo=new int[n+2][n+2];
-        for(int []row:memo){
-            Arrays.fill(row,-1);
-        }
-        int ans=0;
-        solve(0,n-1,s.toCharArray(),memo);
-        for(int i=0;i<n;i+=1){
-           for(int j=i;j<n;j+=1){
-               if(memo[i][j]==1){
-                   ans+=1;
-               }
-           } 
-            
-        }
-        System.out.println(memo[1][1]);
-        return ans;
-    }
+       boolean [][]dp=new boolean[s.length()][s.length()];
+       int ans=0,n=s.length();
+       for(int i=0;i<n;i++){
+          for(int j=0;j<=i;j++){
+              if(s.charAt(i)==s.charAt(j) && (i-j<=2 || dp[j+1][i-1])){
+                  dp[j][i]=true;
+                  ans+=1;
+             }
+          } 
+       } 
+       return ans; 
+   }
 }
