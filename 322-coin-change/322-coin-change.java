5cd021c04b9c2b753b72eae4f85ef9e7e2588571
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-16 21:41:32 +0530

    Time: 46 ms (30.68%), Space: 57.9 MB (14.07%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -19,7 +19,7 @@ class Solution {
      }
     
     public int coinChange(int[] coins, int amount) {
-        Arrays.sort(coins);
+       // Arrays.sort(coins);
         return minCoinUptoAmt(coins,amount);
        //Sort the coins in incresng order
       //Recur reletions
