b930d481f06bf1ee71642fc61dea3b9ff01afd5a
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-19 21:32:58 +0530

    Time: 50 ms (66.11%), Space: 42.7 MB (80.13%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -1,25 +1,17 @@
 class Solution {
-    public int[] perSqUpToN(int n){
-       int []ar=new int[n];
-       ar[0]=1;
-       for(int i=2;i<=n;i++)
-          ar[i-1]=i*i; 
-       return ar;
-    }
-    public int totalPerSqUpToN(int target,int []ar){
-        int [][]dp=new int[ar.length+1][target+1];
-        for(int i=1;i<target+1;i++)
-           dp[0][i]=Integer.MAX_VALUE;
-        for(int i=1;i<ar.length+1;i++){
-          for(int j=1;j<target+1;j++){
-              dp[i][j]=dp[i-1][j];
-              if(j>=ar[i-1])
-                  dp[i][j]=Math.min(dp[i][j],1+dp[i][j-ar[i-1]]);
+  public int totalPerSqUpToN(int target){
+        int []dp=new int[target+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=1;i<target+1;i++){
+          for(int j=1;j*j<=i;j++){
+              int sq=j*j;
+              dp[i]=Math.min(dp[i],1+dp[i-sq]);
            } 
         }
-        return dp[ar.length][target];
+        return dp[target];
     }
     public int numSquares(int n) {
-        return totalPerSqUpToN(n,perSqUpToN(100));
+        return totalPerSqUpToN(n);
     }
 }
