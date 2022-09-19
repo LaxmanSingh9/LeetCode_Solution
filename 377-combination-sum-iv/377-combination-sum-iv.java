dfcfae270a0781c764d9d0f09c3ac1b20698e915
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-19 10:17:31 +0530

    Time: 4 ms (14.12%), Space: 41.1 MB (76.83%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,15 +1,12 @@
 class Solution{
     public int totalWaysSumUptoTarget(int []nums,int target){
         int n=nums.length;
-        Arrays.sort(nums);
         int []dp=new int[target+1];
         dp[0]=1;
         for(int j=1;j<target+1;j++){
            for(int num:nums){
                if(j>=num)
                   dp[j]+=dp[j-num]; 
-               else
-                  break; 
            } 
         }
         return dp[target];
