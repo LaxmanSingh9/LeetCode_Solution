38f40ab1c954fe10ed88c1a06b1ff7a7cab9cb03
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-19 10:21:13 +0530

    Time: 3 ms (29.92%), Space: 42.2 MB (9.09%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,17 +1,19 @@
-class Solution{
-    public int totalWaysSumUptoTarget(int []nums,int target){
-        int n=nums.length;
-        int []dp=new int[target+1];
-        dp[0]=1;
-        for(int j=1;j<target+1;j++){
-           for(int num:nums){
-               if(j>=num)
-                  dp[j]+=dp[j-num]; 
-           } 
-        }
-        return dp[target];
+class Solution {
+    public int totalWaysSumUptoTarget(int []nums,int target,int idx,int[][]memo){
+        if(target==0 || target<0 || idx>=nums.length)
+          return  target==0?1:0;
+        if(memo[idx][target]!=-1)
+            return memo[idx][target];
+        int incl=0,excl=0;
+        if(nums[idx]<=target)
+          incl=totalWaysSumUptoTarget(nums,target-nums[idx],0,memo);
+        excl=totalWaysSumUptoTarget(nums,target,idx+1,memo);
+        return  memo[idx][target]=incl+excl;
     }
     public int combinationSum4(int[] nums, int target) {    
-        return totalWaysSumUptoTarget(nums,target);
+        int [][]memo=new int[nums.length+1][target+1];
+        for(int []ar:memo)
+            Arrays.fill(ar,-1);
+        return totalWaysSumUptoTarget(nums,target,0,memo);
     }
 }
