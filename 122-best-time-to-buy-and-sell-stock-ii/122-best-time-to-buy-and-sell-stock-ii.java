ef19a124bdbdff2748803d65a128f70aa8c4cbb8
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-20 20:04:16 +0530

    Time: 5 ms (13.62%), Space: 48.8 MB (5.01%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -4,12 +4,12 @@ class Solution {
             return 0;
         if(memo[type][idx]!=-1)
            return memo[type][idx]; 
-        int ans1=0,ans2=0;
+        int prfit1=0,prfit2=0;
         if(type==1)
-          ans1=Math.max(-prices[idx]+solve(prices,idx+1,0,memo),solve(prices,idx+1,1,memo));  
+          prfit1=Math.max(-prices[idx]+solve(prices,idx+1,0,memo),solve(prices,idx+1,1,memo));  
         else
-          ans2=Math.max(prices[idx]+solve(prices,idx+1,1,memo),solve(prices,idx+1,0,memo));
-        return memo[type][idx]=Math.max(ans1,ans2);
+          prfit2=Math.max(prices[idx]+solve(prices,idx+1,1,memo),solve(prices,idx+1,0,memo));
+        return memo[type][idx]=Math.max(prfit1,prfit2);
     }
     public int maxProfit(int[] prices) {
        int [][]memo=new int[2][prices.length]; 
