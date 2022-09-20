48a55b17d42d091efd50dcda8d91425b52bd5727
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-20 20:42:23 +0530

    Time: 2 ms (48.76%), Space: 44.6 MB (32.16%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,15 @@
 class Solution {
-    public int solve(int []prices,int idx,int type,int[][]memo){
-        if(idx>=prices.length)
-            return 0;
-        if(memo[type][idx]!=-1)
-           return memo[type][idx]; 
-        int prfit1=0,prfit2=0;
-        if(type==1)
-          prfit1=Math.max(-prices[idx]+solve(prices,idx+1,0,memo),solve(prices,idx+1,1,memo));  
-        else
-          prfit2=Math.max(prices[idx]+solve(prices,idx+1,1,memo),solve(prices,idx+1,0,memo));
-        return memo[type][idx]=Math.max(prfit1,prfit2);
+   public int maxProfit(int[] prices) {
+       int [][]dp=new int[2][prices.length+1];
+       dp[0][0]=Integer.MIN_VALUE;
+       for(int i=1;i<prices.length+1;i++){
+          dp[0][i]=Math.max(dp[0][i-1], -prices[i-1]+dp[1][i-1]);//buy
+          dp[1][i]=Math.max(dp[1][i-1], prices[i-1]+dp[0][i-1]);//sell
+      }
+      return Math.max(dp[0][prices.length],dp[1][prices.length]); 
+     
     }
-    public int maxProfit(int[] prices) {
-       int [][]memo=new int[2][prices.length]; 
-       for(int []ar:memo)
-          Arrays.fill(ar,-1); 
-       return solve(prices,0,1,memo); 
-    }
-     //There are three options on each day either buy on it or sell on it
+     //There are two options on each day either buy on it or sell on it
        //if buy then sell 
        //if sell them buy
        //skip it
