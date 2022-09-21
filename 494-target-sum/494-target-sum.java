977a4e2f05c4267869849deb9722c81d60157a6f
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-21 11:52:01 +0530

    Time: 10 ms (78.70%), Space: 43.5 MB (47.80%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,25 +1,28 @@
 class Solution {
-    public int totalArrSum(int []arr){
-       int sum=0;
-       for(int val:arr)
-          sum+=val; 
-       return sum;
-    }
-    public int solve(int []arr,int target,int idx){
-       if(idx>=arr.length)
-          return target==0?1:0; 
-       //append minus sign
-       int incl=0,excl=0; 
-       incl=solve(arr,target-arr[idx],idx+1);
-       //append plus sign 
-       excl=solve(arr,target,idx+1); 
-       return  incl+excl;
+  public int solve(int []arr,int target){
+      int n=arr.length;  
+      int [][]dp=new int[n+1][target+1];
+      for(int i=0;i<n+1;i++)
+         dp[i][0]=0;
+      for(int i=0;i<target+1;i++)
+         dp[0][i]=0;
+      dp[0][0]=1;
+      for(int i=1;i<n+1;i++){
+        for(int j=0;j<target+1;j++){
+           dp[i][j]+=dp[i-1][j];
+           if(j>=arr[i-1])
+             dp[i][j]+=dp[i-1][j-arr[i-1]];  
+        }  
+      }
+      return dp[n][target];  
         
     }
     public int findTargetSumWays(int[] nums, int target) {
-      int sum=totalArrSum(nums);
+      int sum=0;
+      for(int val:nums)
+          sum+=val; 
       if((target+sum)%2!=0 || Math.abs(target)>sum)return 0;
       target=(sum+target)/2;
-      return solve(nums,target,0);    
+      return solve(nums,target);    
     }
 }
