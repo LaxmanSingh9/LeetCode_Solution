273c063f026b8c24ac9c7df164530a878a01f58b
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-21 10:39:12 +0530

    Time: 919 ms (14.51%), Space: 41.8 MB (74.27%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,24 +1,25 @@
 class Solution {
+    public int totalArrSum(int []arr){
+       int sum=0;
+       for(int val:arr)
+          sum+=val; 
+       return sum;
+    }
+    public int solve(int []arr,int target,int idx){
+       if(idx>=arr.length)
+          return target==0?1:0; 
+       //append minus sign
+       int incl=0,excl=0; 
+       incl=solve(arr,target-arr[idx],idx+1);
+       //append plus sign 
+       excl=solve(arr,target,idx+1); 
+       return  incl+excl;
+        
+    }
     public int findTargetSumWays(int[] nums, int target) {
-        int sum=0,n=nums.length;
-        for(int val:nums)sum+=val;
-        if((target+sum)%2!=0 || Math.abs(target)>sum)return 0;
-        target=(sum+target)/2;
-        int [][]dp=new int[n+1][target+1];
-        dp[0][0]=1;
-        for(int j=1;j<target+1;j+=1)
-          dp[0][j]=0;
-        for(int i=1;i<n+1;i+=1){
-          for(int j=0;j<target+1;j+=1){
-              if(nums[i-1]<=j)
-                 dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
-              else
-                 dp[i][j]=dp[i-1][j];
-          }
-        }
-        //System.out.println(dp[0][0]+" "+dp[0][1]);
-        return dp[n][target];
-        
-        
+      int sum=totalArrSum(nums);
+      if((target+sum)%2!=0 || Math.abs(target)>sum)return 0;
+      target=(sum+target)/2;
+      return solve(nums,target,0);    
     }
 }
