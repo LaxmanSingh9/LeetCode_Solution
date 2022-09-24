7c57ba7d35a4b6f86c23d6606638b01de44dc403
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-24 22:34:57 +0530

    Time: 13 ms (74.89%), Space: 47.6 MB (70.59%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -4,15 +4,18 @@ class Solution {
            if(ar1[i]!=ar2[i])return false;
         return true;
     } 
-    public List<Integer> findAnagrams(String s, String p) {
-       int pLen=p.length(),sLen=s.length();List<Integer>allAnagStIdx=new ArrayList<>();
-       int []freqP=new int[26]; int []freqS=new int[26];
-       for(char ch:p.toCharArray())freqP[ch-97]+=1;
+    public List<Integer> findAnagrams(String s, String p){
+       List<Integer>allAnagStIdx=new ArrayList<>(); 
+       int pLen=p.length();int []freqP=new int[26];
+       int sLen=s.length();int []freqS=new int[26];
+       for(char ch:p.toCharArray())
+           freqP[ch-97]+=1;
        int st=0,end=0;
        for(int i=0;i<sLen;i++){
            freqS[s.charAt(i)-97]+=1;
            if(end-st+1==pLen) {
-               if(hasSameFreq(freqS,freqP))allAnagStIdx.add(st);
+               if(hasSameFreq(freqS,freqP))
+                   allAnagStIdx.add(st);
                freqS[s.charAt(st)-97]-=1;
                st+=1;
            }
