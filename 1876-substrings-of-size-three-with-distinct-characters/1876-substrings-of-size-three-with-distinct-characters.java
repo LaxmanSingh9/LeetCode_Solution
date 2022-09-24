da76bc5f1b93fefa4a31be56826dff7a728e43fd
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-24 16:23:48 +0530

    Time: 20 ms (5.27%), Space: 40.1 MB (99.03%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAllCharUnique(Map<Character,Integer>map){
+        int uniq=0;
+        for(char ch:map.keySet()){
+            if(map.get(ch)>0)
+               uniq+=1; 
+        }
+        return uniq==3?true:false;
+    }
+    public int countGoodSubstrings(String s) {
+       int i=0,j=0,ans=0,subLen=3;
+       Map<Character,Integer>map=new HashMap<>(); 
+       while(j<s.length()){
+           char ch=s.charAt(j); 
+           map.put(ch,map.getOrDefault(ch,0)+1);
+           if(j-i+1==subLen){
+               ans+=isAllCharUnique(map)?1:0;ch=s.charAt(i);
+               map.put(ch,map.get(ch)-1); 
+               i+=1;
+           }
+           j+=1;
+       }
+       return ans; 
+        
+    }
+}
