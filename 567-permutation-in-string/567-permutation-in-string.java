30843dc66df68a0dff94936c6fd26106c5b3211c
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-25 09:33:39 +0530

    Time: 25 ms (45.24%), Space: 42.6 MB (85.92%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,32 +1,26 @@
 class Solution {
-    public boolean hasSameFreq(int[]ar1,int []ar2){
+    public boolean hasAllZeros(int[]ar1){
        for(int i=0;i<ar1.length;i++){
-          if(ar1[i]!=ar2[i])
+          if(ar1[i]!=0)
              return  false;
        }
        return true; 
     }
     public boolean checkInclusion(String s1, String s2) {
-       int []s1Freq=new int[26];
-       int []s2Freq=new  int[26];
-       for(char ch:s1.toCharArray())
-          s1Freq[ch-'a']+=1; 
-       int st=0; 
-       for(int e=0;e<s2.length();e++){
-           char ch=s2.charAt(e);
-           s2Freq[ch-'a']+=1;
-           if(e-st+1==s1.length()){
-               if(hasSameFreq(s1Freq,s2Freq))
-                  return true;
-               s2Freq[s2.charAt(st)-'a']-=1;
-               st+=1; 
-           } 
+       if(s1.length()>s2.length())
+          return false; 
+       int []freq=new int[26];
+       for(int i=0;i<s1.length();i++) {
+           freq[s1.charAt(i)-'a']++;freq[s2.charAt(i)-'a']--;
+       }
+       if(hasAllZeros(freq))
+          return true; 
+       for(int e=s1.length();e<s2.length();e++){
+           freq[s2.charAt(e)-'a']--;
+           freq[s2.charAt(e-s1.length())-'a']++;
+           if(hasAllZeros(freq))
+              return true; 
        } 
-       return false; 
-        
-       //will need to find an substring which has sames chars as s1 has
-       //run an loop as long as find an sub string of lenght s1 comapre the char freq of both string
-       //TC O(n)
-       //SC(2*26) 
+       return false;
     }
 }
