1d17d4646eb8d8a615b7a57f49f067953fe41fd3
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-26 21:28:37 +0530

    Time: 136 ms (19.37%), Space: 114 MB (18.89%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -3,19 +3,16 @@ class Solution {
        if(fruits.length==1)
            return fruits.length;
        List<Integer>toDel;
-       Map<Integer,Integer>map=new HashMap<>(); 
+       Map<Integer,Integer>freq=new HashMap<>(); 
        int maxPick=0,i=0; 
        for(int j=0;j<fruits.length;j++){
-           int val=fruits[j];
-           map.put(val,map.getOrDefault(val,0)+1);
-           if(map.size()>2){
-              toDel=new ArrayList<>(); 
-              while(map.size()-toDel.size()>2){
-                 map.put(fruits[i],map.get(fruits[i])-1);
-                 if(map.get(fruits[i])==0)toDel.add(fruits[i]);
+           freq.put(fruits[j],freq.getOrDefault(fruits[j],0)+1);
+           if(freq.size()>2){
+              while(freq.size()>2){
+                 freq.put(fruits[i],freq.get(fruits[i])-1);
+                 if(freq.get(fruits[i])==0)freq.remove(fruits[i]);
                  i+=1;
               }
-              for(int v:toDel)map.remove(v);
            }
            maxPick=Math.max(j-i+1,maxPick);
        }
