db4c5884c66e08a10c7bddfb0b6ea1037b3dd787
Laxman Singh <55058984+LaxmanSingh9@users.noreply.github.com>
2022-09-29 09:37:09 +0530

    Time: 1 ms (78.63%), Space: 42.4 MB (68.94%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,26 +1,27 @@
 class Solution {
-    public int pivot(int []nums,int val,int st,int end){
-        if(end<=st)
-          return  end;
-        int mid=(end+st)/2;
-        if(val<=nums[mid])return pivot(nums,val,mid+1,end);
-        else return pivot(nums,val,st,mid-1);
-    }
-    int binarySearch(int arr[], int l, int r, int x){
-        if(r>=l){
-            int mid = l + (r - l) / 2;
-            if (arr[mid]==x)return mid;
-            if (arr[mid]>x)return binarySearch(arr, l, mid - 1, x);
-            return binarySearch(arr, mid + 1, r, x);
-        }
-        return -1;
-    }
     public int search(int[] nums, int target) {
-       int k=pivot(nums,nums[0],0,nums.length-1);
-       if(k+1<nums.length && nums[k]>nums[k+1])k+=1;
-       int a=binarySearch(nums,k,nums.length-1,target);
-       if(a!=-1)return a;
-       int b=binarySearch(nums,0,k,target);
-       return b; 
+    int lo = 0, hi = nums.length - 1;
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+        
+        int num = nums[mid];
+        // If nums[mid] and target are "on the same side" of nums[0], we just take nums[mid].
+        if ((nums[mid] < nums[0]) == (target < nums[0])) {
+            num = nums[mid];
+        } else if(nums[mid]>=nums[0]==(target>=nums[0])){
+            num=nums[mid];
+        }
+        else {
+            num = target < nums[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+
+        if (num < target)
+            lo = mid + 1;
+        else if (num > target)
+            hi = mid - 1;
+        else
+            return mid;
     }
+    return -1;
+}
 }
